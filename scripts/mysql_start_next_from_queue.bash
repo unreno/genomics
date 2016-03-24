f000ec8b7f203fe766d88d7d51395df333e3d999
Jake <github@jakewendt.com>
2016-03-24 13:11:10 -0700

    Meddling with mysql as more powerful SQS.

--- scripts/mysql_start_next_from_queue.bash
+++ scripts/mysql_start_next_from_queue.bash
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
-mysql --user root << EOF
-CONNECT queue;
 
+
+mysql --user root queue << EOF
 LOCK TABLES queue WRITE;
-SELECT * FROM queue;
-SELECT SLEEP(10);
+
+SELECT id INTO @last FROM queue
+WHERE started_at = 0
+ORDER BY added_at ASC
+LIMIT 1;
+
+UPDATE queue
+SET started_at = CURRENT_TIMESTAMP
+WHERE id = @last;
+
+SELECT * FROM queue
+WHERE id = @last;
+
 UNLOCK TABLES;
 EOF
 
