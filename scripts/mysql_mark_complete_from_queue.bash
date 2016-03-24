4623103175d29a14a9b64531dc03acf509a1994e
Jake <github@jakewendt.com>
2016-03-24 15:47:32 -0700

    Added mysql_mark_complete_from_queue.bash

---
+++ scripts/mysql_mark_complete_from_queue.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+var="LOCK TABLES queue WRITE;"
+
+while [ $# -ne 0 ] ; do
+	var="${var}"$'\n'"UPDATE queue SET completed_at = CURRENT_TIMESTAMP WHERE id = '$1';"
+	shift
+done
+
+var="${var}"$'\n'"UNLOCK TABLES;"
+
+echo "$var"
+
+echo "$var" | mysql --user root queue
