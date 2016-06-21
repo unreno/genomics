f78472b07c29938691d1bd07b2be822df2fe9d21
Jake <github@jakewendt.com>
2016-06-21 15:03:24 -0700

    Fine tuning plink scripts.

--- scripts/aws_plink_queue_commands.bash
+++ scripts/aws_plink_queue_commands.bash
@@ -2,15 +2,20 @@
 
 basedir=~/plinking/test/input
 
+#	Aliases don't work here, but redefining it should
+echo "alias awsq='mysql_queue.bash --defaults_file ~/.awsqueue.cnf'"
+
+#	Recommend piping the output to sh
+
 #for population in `ls -1d $basedir/pheno_files/* | xargs -n 1 basename` ; do
 #for population in eur amr afr eas sas ; do	#	5
 for population in eur ; do
 
 	for pheno_name in `ls -1d $basedir/pheno_files/$population/* | xargs -n 1 basename` ; do
 	
-		for chromosome in `seq 1 23 && echo X && echo Y` ; do
+		for chromosome in `seq 1 22 && echo X && echo Y` ; do
 
-			echo "aws_plink_wrapper.bash ${population} ${pheno_name} ${chromosome}"
+			echo "awsq push 'aws_plink_wrapper.bash ${population} ${pheno_name} ${chromosome}'"
 
 		done
 	done
--- scripts/aws_plink_wrapper.bash
+++ scripts/aws_plink_wrapper.bash
@@ -13,49 +13,55 @@ function usage(){
 }
 [ $# -ne 3 ] && usage
 
-mkdir -p ~/references/
+BASE=~/snpprocessing
+REFS=$BASE/references
+WORK=$BASE/working
 
-[ -f ~/references/1kg_all_chroms_pruned_mds.mds ] ||
-	aws s3 cp s3://herv/snp-20160620/1kg_all_chroms_pruned_mds.mds \
-		~/references/
-#	cp ~/plinking/test/input/1kg_all_chroms_pruned_mds.mds \
-#		~/references/
+#	It would be nice to avoid repeated copying, but that would take up much space.
+#	So, start fresh every go
+/bin/rm -rf $BASE
 
-#/bin/rm -rf ~/working/
-mkdir -p ~/working/
-cd ~/working/
+mkdir -p $REFS
+
+[ -f $REFS/1kg_all_chroms_pruned_mds.mds ] ||
+	aws s3 cp s3://herv/snp-20160620/1kg_all_chroms_pruned_mds.mds $REFS/
+#	cp ~/plinking/test/input/1kg_all_chroms_pruned_mds.mds $REFS
+
+#/bin/rm -rf $WORK
+mkdir -p $WORK
+cd $WORK
 
 population=$1
 pheno_name=$2
 chromosome=$3
 
-mkdir -p ~/references/pheno_files/${population}
-[ -f ~/references/pheno_files/${population}/${pheno_name} ] ||
+mkdir -p $REFS/pheno_files/${population}
+[ -f $REFS/pheno_files/${population}/${pheno_name} ] ||
 	aws s3 cp s3://herv/snp-20160620/pheno_files/${population}/${pheno_name} \
-		~/references/pheno_files/${population}/
+		$REFS/pheno_files/${population}/
 #	cp ~/plinking/test/input/pheno_files/$population/$pheno_name \
-#		~/references/pheno_files/$population/
+#		$REFS/pheno_files/$population/
 
 
-mkdir -p ~/references/pruned_vcfs/${population}
+mkdir -p $REFS/pruned_vcfs/${population}
 #	S3 and wildcards are challenging. Copy a dir, exclude everything, include only what you want.
 #	Not elegant, but effective
 #	--exclude "*" --include "*.log"
 #	aws s3 cp s3://herv/snp-20160620/pruned_vcfs/$population/ --recursive --exclude "*" --include "ALL.chr${chromosome}.*.tar.gz" ./
-[ -f ~/references/pruned_vcfs/${population}/ALL.chr${chromosome}.*bed ] ||
+[ -f $REFS/pruned_vcfs/${population}/ALL.chr${chromosome}.*bed ] ||
 	aws s3 cp s3://herv/snp-20160620/pruned_vcfs/$population/ALL.chr${chromosome}.tar.gz \
-		~/references/pruned_vcfs/${population}/
-#		~/references/pruned_vcfs/${population}/ --recursive \
+		$REFS/pruned_vcfs/${population}/
+#		$REFS/pruned_vcfs/${population}/ --recursive \
 #		--exclude "*" --include "ALL.chr${chromosome}.*.tar.gz"
 #	cp ~/plinking/test/input/pruned_vcfs/${population}/ALL.chr${chromosome}.*.gz \
-#		~/references/pruned_vcfs/${population}/
+#		$REFS/pruned_vcfs/${population}/
 
 #	untar/gunzip bed,bim,fam files
-[ -f ~/references/pruned_vcfs/${population}/ALL.chr${chromosome}.*bed ] ||
-	tar -xvzC ~/references/pruned_vcfs/${population}/ \
-		-f ~/references/pruned_vcfs/${population}/ALL.chr${chromosome}.*.gz
+[ -f $REFS/pruned_vcfs/${population}/ALL.chr${chromosome}.*bed ] ||
+	tar -xvzC $REFS/pruned_vcfs/${population}/ \
+		-f $REFS/pruned_vcfs/${population}/ALL.chr${chromosome}.*.gz
 
-bedfile=`ls ~/references/pruned_vcfs/${population}/ALL.chr${chromosome}.*bed`
+bedfile=`ls $REFS/pruned_vcfs/${population}/ALL.chr${chromosome}.*bed`
 bfile=${bedfile%.*} # drop the shortest suffix match to ".*" (the .bed extension)
 bfile_base=${bedfile##*/}	#	drop the longest prefix match to "*/" (the path)
 out_base_path="${bfile_base}.${population}"
@@ -66,25 +72,27 @@ plink --snps-only \
 		--logistic hide-covar \
 		--covar-name C1,C2,C3,C4,C5,C6 \
 		--bfile ${bfile} \
-		--pheno ~/references/pheno_files/${population}/${pheno_name} \
+		--pheno $REFS/pheno_files/${population}/${pheno_name} \
 		--out ${out_base_path}.no.covar \
-		--covar ~/references/1kg_all_chroms_pruned_mds.mds
+		--covar $REFS/1kg_all_chroms_pruned_mds.mds
 
 #awk -F" " '{print $1,$2,$3,$9,$4,$7}' ${out_base_path}.no.covar.assoc.logistic > ${out_base_path}.for.plot.txt
 #	space is default separator
 awk '{print $1,$2,$3,$9,$4,$7}' ${out_base_path}.no.covar.assoc.logistic > ${out_base_path}.for.plot.txt
 
-gzip --best ${out_base_path}.for.plot.txt
+gzip --best ${out_base_path}.no.covar.log
+aws s3 cp ${out_base_path}.no.covar.log.gz \
+	s3://herv/snp-20160620/output/${population}/${pheno_name}/
 
+gzip --best ${out_base_path}.for.plot.txt
 aws s3 cp ${out_base_path}.for.plot.txt.gz \
 	s3://herv/snp-20160620/output/${population}/${pheno_name}/
-#${out_base_path}.for.plot.txt.gz
 
 #	copy out plot.txt file to s3?
 
 #	tar gzip plot.txt
 #	aws s3 cp plot.txt s3://herv/snp-20160620/output/${population}/${pheno_name}/${bfile}
 
-#cd ~/
-#/bin/rm -rf ~/working/
+cd ~/
+/bin/rm -rf $BASE
 
