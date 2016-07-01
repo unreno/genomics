7613d170888f86526df2a9c09fb1dacc6ddf3fa6
Jake <github@jakewendt.com>
2016-07-01 15:52:05 -0700

    Adjusted plink command queueing.

--- scripts/aws_plink_queue_commands.bash
+++ scripts/aws_plink_queue_commands.bash
@@ -12,14 +12,24 @@ echo "alias awsq='mysql_queue.bash --defaults_file ~/.awsqueue.cnf'"
 for population in eur amr afr eas sas ; do	#	5
 #for population in eur ; do
 
-	for pheno_name in `ls -1d $basedir/pheno_files/$population/* | xargs -n 1 basename` ; do
+	for genome in hg19_alt ; do
+
+
+#	Given file sizes and such, it'd be better if genome were in population dir
+#		rather than population in genome dir.
+#	Then during processing the population files wouldn't have to be downloaded multiple times.
+
+
+
+		for pheno_name in `ls -1d ${basedir}/pheno_files/${genome}/${population}/* | xargs -n 1 basename` ; do
 	
 #		for chromosome in `seq 1 22 && echo X && echo Y` ; do
 
-			echo "awsq push 'aws_plink_wrapper.bash --genome hg19_alt --pop ${population} --pheno ${pheno_name}"
+			echo "awsq push 'aws_plink_wrapper.bash --genome ${genome} --pop ${population} --pheno ${pheno_name}"
 #			echo "awsq push 'aws_plink_wrapper.bash --genome hg19_alt --pop ${population} --pheno ${pheno_name} --chromosome ${chromosome}'"
 #			echo "awsq push 'aws_plink_wrapper.bash ${population} ${pheno_name} ${chromosome}'"
 
-#		done
+		done
 	done
 done
+
