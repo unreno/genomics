a44bc0dbb7ce886b485ed3cd05e0e27b5c4f5aa5
Jake <github@jakewendt.com>
2016-07-07 16:52:16 -0700

    Added latest AMI as default

--- scripts/aws/create_ec2_instance.bash
+++ scripts/aws/create_ec2_instance.bash
@@ -49,7 +49,8 @@ function usage(){
 #	image_id="ami-3cbe7e5c"	#	Base HERV 5
 #	image_id="ami-10c40470"	#	Base HERV 6
 #	image_id="ami-f13efe91"	#	HERV 7
-image_id="ami-d35494b3"	#	HERV 8
+#	image_id="ami-d35494b3"	#	HERV 8
+image_id="ami-d05595b0"	#	HERV 9
 instance_type="t2.micro"
 #instance_type="t2.medium"
 key="~/.aws/KEYNAME.pem"
