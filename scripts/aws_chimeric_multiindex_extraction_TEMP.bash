f6b928a94f45a36e9918f1200d851b35a622d276
Jake <github@jakewendt.com>
2016-07-23 19:40:18 -0700

    Corrected find call.

--- scripts/aws_chimeric_multiindex_extraction_TEMP.bash
+++ scripts/aws_chimeric_multiindex_extraction_TEMP.bash
@@ -55,7 +55,7 @@ done
 
 initial_PWD=$PWD
 #for file in `find . -name \*pre\*fasta` ; do
-for file in `find . -type d -maxdepth 1` ; do
+for file in `find . -mindepth 1 -maxdepth 1 -type d` ; do
 	cd $initial_PWD
 #	cd `dirname $file`
 	cd $file
