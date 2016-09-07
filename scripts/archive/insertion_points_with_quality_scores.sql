1e26bc252b49f4d743f7be62851a6654037bf75e
Jake <github@jakewendt.com>
2016-09-07 16:40:01 -0700

    Added another table for joining and selection.

--- scripts/archive/insertion_points_with_quality_scores.sql
+++ scripts/archive/insertion_points_with_quality_scores.sql
@@ -26,7 +26,7 @@ CREATE INDEX position ON insertion_points ( position );
 --	INTO TABLE insertion_points_with_quality_scores.insertion_points
 --	FIELDS TERMINATED BY ','
 --	IGNORE 1 LINES;
---	
+--
 
 --ALTER TABLE insertion_points CHANGE insertion_point_name name VARCHAR(20);
 --ALTER TABLE insertion_points CHANGE map_quality_score q_score INT;
@@ -50,4 +50,43 @@ INTO OUTFILE '/tmp/points.csv'
 
 --WHERE chromosome = 'chrY'
 --HAVING AVG(q_score) > 10
+
+
+
+
+
+CREATE TABLE input (
+	siteq00 VARCHAR(50),
+	prevalence FLOAT,
+	meanNreads FLOAT,
+	meanQ FLOAT,
+	rangeQ FLOAT
+);
+
+CREATE INDEX siteq00 ON input ( siteq00 );
+
+--	This file is from Amelia. Must convert the windows CRLF.
+--
+--	LOAD DATA INFILE '/Users/jakewendt/Downloads/q00_site_quality_scores.csv'
+--	INTO TABLE insertion_points_with_quality_scores.input
+--	FIELDS TERMINATED BY ','
+--	IGNORE 1 LINES;
+--
+
+SELECT 'name', 'name_count', 'subject_count',
+	'ave_per_subject',
+	'aveQ', 'minQ', 'maxQ',
+	'stdQ', 'varQ'
+UNION
+SELECT name, COUNT(name) AS name_count,
+	COUNT(DISTINCT subject_id) AS subject_count,
+	TRUNCATE(COUNT(subject_id)/COUNT(DISTINCT subject_id),3) AS ave_per_subject,
+	TRUNCATE(AVG(q_score), 3) AS aveQ, MIN(q_score) AS minQ, MAX(q_score) AS maxQ,
+	TRUNCATE(STDDEV_POP(q_score),3) AS stdQ, TRUNCATE(VAR_POP(q_score),3) AS varQ
+FROM input i
+OUTER JOIN insertion_points p ON i.siteq00 = p.name
+GROUP BY name
+INTO OUTFILE '/tmp/points.csv'
+	FIELDS TERMINATED BY ','
+	LINES TERMINATED BY '\n';
 
