940e974e53b82663d77cbeaeb9c116ed0347c3d6
Jake <github@jakewendt.com>
2017-02-09 10:02:51 -0800

    Meddling with one-off script.

--- scripts/archive/20170208_getGenes.R
+++ scripts/archive/20170208_getGenes.R
@@ -2,12 +2,46 @@
 #source("https://bioconductor.org/biocLite.R")
 #biocLite("ballgown")
 
-library(ballgown)
+#	wget https://www.dropbox.com/s/4gd05ghjkurj170/Homo_sapiens.GRCh37.73_chrPrefix.gtf
 
+library(ballgown)
 
 load("eQTL_GEUVADIS_imputed_list_cis_1e6_snpM)IKZF.rda")
 load("eqtl_rs2290400.rda")
 load("gownTransMap2.rda")
+
+## annotate to Ensembl genes
+gtf="Homo_sapiens.GRCh37.73_chrPrefix.gtf"
+genes = getGenes(gtf, gownTransMap2,attribute = "gene_name")
+names(genes) = names(gownTransMap2)
+eqtl$geneSymbol = genes[match(eqtl$gene, names(genes))]
+
+genes2 = getGenes(gtf, gownTransMap2,attribute = "gene_id")
+names(genes2) = names(gownTransMap2)
+eqtl$ensemblGeneID = genes2[match(eqtl$gene, names(genes2))]
+
+sig = eqtl[eqtl$FDR < 0.05,]
+
+save(sig, file="sig_eQTL_GEUVADIS_imputed_list_cis_1e6_annotated_.rda" ,compress=TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 me<-me2
 
@@ -28,20 +62,4 @@ eqtl$transEnd = genepos$end[match(eqtl$gene, genepos$txID)]
 
 eqtl$distStartMinusSnp = eqtl$transStart - eqtl$snpPos
 eqtl = DataFrame(eqtl)
-
-## annotate to Ensembl genes
-gtf="Homo_sapiens.GRCh37.73_chrPrefix.gtf"
-genes = getGenes(gtf, gownTransMap2,attribute = "gene_name")
-names(genes) = names(gownTransMap2)
-eqtl$geneSymbol = genes[match(eqtl$gene, names(genes))]
-
-genes2 = getGenes(gtf, gownTransMap2,attribute = "gene_id")
-names(genes2) = names(gownTransMap2)
-eqtl$ensemblGeneID = genes2[match(eqtl$gene, names(genes2))]
-
-sig = eqtl[eqtl$FDR < 0.05,]
-
-save(sig, file="sig_eQTL_GEUVADIS_imputed_list_cis_1e6_annotated_.rda" ,compress=TRUE)
-
-
 
