9f39556909e2f1a6076eb3e9df5b140a881db6e6
Jake <github@jakewendt.com>
2017-10-26 16:22:28 -0700

    Refining and testing locally.

--- scripts/aws_1000genomes_extract_locally_aligned.bash
+++ scripts/aws_1000genomes_extract_locally_aligned.bash
@@ -4,6 +4,7 @@ script_name=$(basename $0)
 reference=SVAs_and_HERVs_KWHE
 #size=100
 threads=4
+do_aws_stuff=true
 
 function usage(){
 	echo
@@ -12,12 +13,12 @@ function usage(){
 	echo "Usage: (for 1000 genomes data on S3)"
 	echo
 #	echo "$script_name [--reference BOWTIE2_INDEX] [--size GB_EBS_DISK_TO_USE] SUBJECT_NAME SAMPLE_NAME"
-	echo "$script_name [--reference BOWTIE2_INDEX] SUBJECT_NAME SAMPLE_NAME"
+	echo "$script_name [--reference BOWTIE2_INDEX] SAMPLE_NAME RUN_ID"
 	echo
 	echo "Default:"
 	echo "  reference .... ${reference}"
 #	echo "  size ......... ${size}"
-	echo "  subject ...... ${subject}"
+#	echo "  subject ...... ${subject}"
 	echo
 	echo "$script_name --reference SVAs_and_HERVs NA18566 ERR000069"
 	echo
@@ -38,6 +39,8 @@ while [ $# -ne 0 ] ; do
 #			shift; population=$1; shift ;;
 #		-ph*|--ph*)
 #			shift; pheno_name=$1; shift ;;
+		--skip_aws_stuff)
+			shift; do_aws_stuff=false ;;
 		--)	#	just -- is a common and explicit "stop parsing options" option
 			shift; break ;;
 		-*)
@@ -49,23 +52,25 @@ done
 
 [ $# -ne 2 ] && usage
 
-subject=$1
-sample=$2
+sample_name=$1
+run_id=$2
 
 
+WORK=$HOME/work
+TMP=$WORK/tmp
 
 #	Try to add some uniqueness to log file name
 date=`date "+%Y%m%d%H%M%S"`
 #log=$HOME/$script_name.$date.`hostname`.$1.$2.$$.log
-log=$HOME/$script_name.$1.$2.$date.log
+log=$WORK/$script_name.$sample_name.$run_id.$date.log
 
 S3=s3://herv/1000genomes/${reference}
 
-WORK=$HOME/work
-	
+
 /bin/rm -rf $WORK
 
 mkdir -p $WORK
+mkdir -p $TMP
 cd $WORK
 
 #set -v
@@ -77,188 +82,199 @@ set -x
 	date
 
 
-#	Check the sizes of the input files and compute needed EBS workspace.
 
-#	There aren't any intermediate files. All computation is in memory.
-#	Output to main drive so this EBS disk just needs to be large enough to hold these 2 files.
-#	No computation aside from addition is needed.
+	if [ $do_aws_stuff == 'true' ] ; then
 
-#	aws s3 ls s3://1000genomes/phase3/data/NA21130/sequence_read/ERR250999_2.filt.fastq.gz | awk '{print $3}'
-#3237919545
+		#	Check the sizes of the input files and compute needed EBS workspace.
 
-s1=$(aws s3 ls s3://1000genomes/phase3/data/${subject}/sequence_read/${sample}_1.filt.fastq.gz | awk '{print $3}')
-s2=$(aws s3 ls s3://1000genomes/phase3/data/${subject}/sequence_read/${sample}_2.filt.fastq.gz | awk '{print $3}')
+		#	There aren't any intermediate files. All computation is in memory.
+		#	Output to main drive so this EBS disk just needs to be large enough to hold these 2 files.
+		#	No computation aside from addition is needed.
 
-echo $s1, $s2
+		#	aws s3 ls s3://1000genomes/phase3/data/NA21130/sequence_read/ERR250999_2.filt.fastq.gz | awk '{print $3}'
+		#3237919545
 
-#echo $[$[622797752+622797753]/1000000]
-#1245
+		s1=$(aws s3 ls s3://1000genomes/phase3/data/${sample_name}/sequence_read/${run_id}_1.filt.fastq.gz | awk '{print $3}')
+		s2=$(aws s3 ls s3://1000genomes/phase3/data/${sample_name}/sequence_read/${run_id}_2.filt.fastq.gz | awk '{print $3}')
 
-ebs_size=$[$[$[$s1+$s2]/1000000000]+2]
-echo $ebs_size
+		echo $s1, $s2
 
-#	The will take some testing to determine exact usage
+		#echo $[$[622797752+622797753]/1000000]
+		#1245
 
-#	The size of the volume, in GiBs.
+		ebs_size=$[$[$[$s1+$s2]/1000000000]+2]
+		echo $ebs_size
 
+		#	The will take some testing to determine exact usage
 
-az=$( curl http://169.254.169.254/latest/meta-data/placement/availability-zone/ )
-
-#curl http://169.254.169.254/latest/dynamic/instance-identity/document
-#{
-#  "devpayProductCodes" : null,
-#  "privateIp" : "172.31.11.163",
-#  "availabilityZone" : "us-west-2c",
-#  "version" : "2010-08-31",
-#  "instanceId" : "i-0905ad558e681257c",
-#  "billingProducts" : null,
-#  "instanceType" : "t2.micro",
-#  "pendingTime" : "2017-10-25T19:56:48Z",
-#  "accountId" : "372018666448",
-#  "architecture" : "x86_64",
-#  "kernelId" : null,
-#  "ramdiskId" : null,
-#  "imageId" : "ami-665f9006",
-#  "region" : "us-west-2"
-#}
-region=$( curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq '.region' | tr -d '"' )
+		#	The size of the volume, in GiBs.
 
 
-#WHY are region AND availability zone needed? If az is given, region can only be 1 thing!
+		az=$( curl http://169.254.169.254/latest/meta-data/placement/availability-zone/ )
 
-command="aws ec2 create-volume --region ${region} --availability-zone ${az} --volume-type gp2 --size ${ebs_size}"
-echo $command
-response=$( $command )
-echo "$response"
-volume_id=$( echo "$response" | jq '.VolumeId' | tr -d '"' )
-$echo $volume_id
-#	vol-1234567890abcdef0
-
-
-aws ec2 describe-volumes --region ${region} --volume-id $volume_id
-
-
-instance_id=$( curl http://169.254.169.254/latest/meta-data/instance-id )
-
-#	device names ... /dev/sd[f-p]
-
-command="aws ec2 attach-volume --region ${region} --device xvdf --instance-id ${instance_id} --volume-id ${volume_id}"
-echo $command
-response=$( $command )
-echo "$response"
+		#curl http://169.254.169.254/latest/dynamic/instance-identity/document
+		#{
+		#  "devpayProductCodes" : null,
+		#  "privateIp" : "172.31.11.163",
+		#  "availabilityZone" : "us-west-2c",
+		#  "version" : "2010-08-31",
+		#  "instanceId" : "i-0905ad558e681257c",
+		#  "billingProducts" : null,
+		#  "instanceType" : "t2.micro",
+		#  "pendingTime" : "2017-10-25T19:56:48Z",
+		#  "accountId" : "372018666448",
+		#  "architecture" : "x86_64",
+		#  "kernelId" : null,
+		#  "ramdiskId" : null,
+		#  "imageId" : "ami-665f9006",
+		#  "region" : "us-west-2"
+		#}
+		region=$( curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq '.region' | tr -d '"' )
 
 
+		#WHY are region AND availability zone needed? If az is given, region can only be 1 thing!
+
+		command="aws ec2 create-volume --region ${region} --availability-zone ${az} --volume-type gp2 --size ${ebs_size}"
+		echo $command
+		response=$( $command )
+		echo "$response"
+		volume_id=$( echo "$response" | jq '.VolumeId' | tr -d '"' )
+		$echo $volume_id
+		#	vol-1234567890abcdef0
 
 
+		aws ec2 describe-volumes --region ${region} --volume-id $volume_id
 
-#	as it is possible that some mount points may linger, check ?
-#	[ -e /dev/xvdf ] ....
+
+		instance_id=$( curl http://169.254.169.254/latest/meta-data/instance-id )
+
+		#	device names ... /dev/sd[f-p]
+
+		command="aws ec2 attach-volume --region ${region} --device xvdf --instance-id ${instance_id} --volume-id ${volume_id}"
+		echo $command
+		response=$( $command )
+		echo "$response"
 
 
 
-sudo file -s /dev/xvdf
-#	-> data
-#	(/dev/sdf would be a link so not helpful)
 
 
-#	Need to create file system on volume
-sudo mkfs -t ext4 /dev/xvdf
-
-
-sudo file -s /dev/xvdf
-#	-> /dev/xvdf: Linux rev 1.0 ext4 filesystem data, UUID=0366fdd8-7691-4b3f-be37-2b03a33586a9 (extents) (large files) (huge files)
-
-
-mkdir ~/tmp
-sudo mount /dev/xvdf ~/tmp
-
-
-df -h ~/tmp
-#	Filesystem      Size  Used Avail Use% Mounted on
-#	/dev/xvdf       976M  1.3M  908M   1% /home/ec2-user/ebsdrive
-
-#	Note the loss of 1%
-
-
-#	Owned by root so only writable by root unless ...
-sudo chmod 777 ~/tmp
+		#	as it is possible that some mount points may linger, check ?
+		#	[ -e /dev/xvdf ] ....
 
 
 
-aws s3 cp s3://1000genomes/phase3/data/${subject}/sequence_read/${sample}_1.filt.fastq.gz  ~/tmp/
-aws s3 cp s3://1000genomes/phase3/data/${subject}/sequence_read/${sample}_2.filt.fastq.gz  ~/tmp/
+		sudo file -s /dev/xvdf
+		#	-> data
+		#	(/dev/sdf would be a link so not helpful)
 
 
-#	--xeq is irrelevant here as aren't keeping the sam/bam output.
+		#	Need to create file system on volume
+		sudo mkfs -t ext4 /dev/xvdf
 
-#	bowtie2 --xeq --very-sensitive-local --threads $threads -x $reference \
-#		-q -1 ${sample}_1.filt.fastq.gz -2 ${sample}_2.filt.fastq.gz \
-#		| gawk -F"\t" '
-##			BEGIN { OFS="\t" }
-#			( /^@/ ){ print; next; }
-#			( !and($2,4) || !and($2,8) ){ 
-##	Add aligned reference to read name? Or not?
-##				$1=$1 "_" $3
-#				print }
-#		' \
-#		| samtools fastq -c 9 -1 ${sample}_1.${reference}.fastq.gz -2 ${sample}_2.${reference}.fastq.gz -
-#
+
+		sudo file -s /dev/xvdf
+		#	-> /dev/xvdf: Linux rev 1.0 ext4 filesystem data, UUID=0366fdd8-7691-4b3f-be37-2b03a33586a9 (extents) (large files) (huge files)
+
+
+		sudo mount /dev/xvdf $TMP
+
+
+		df -h $TMP
+		#	Filesystem      Size  Used Avail Use% Mounted on
+		#	/dev/xvdf       976M  1.3M  908M   1% /home/ec2-user/ebsdrive
+
+		#	Note the loss of 1%
+
+
+		#	Owned by root so only writable by root unless ...
+		sudo chmod 777 $TMP
+
+	fi
+
+	aws s3 cp s3://1000genomes/phase3/data/${sample_name}/sequence_read/${run_id}_1.filt.fastq.gz  $TMP
+	aws s3 cp s3://1000genomes/phase3/data/${sample_name}/sequence_read/${run_id}_2.filt.fastq.gz  $TMP
+
+
+	: ${BOWTIE2_INDEXES:="$HOME/BOWTIE2_INDEXES"}
+	#       they MUST be exported, apparently, to be picked up by bowtie2
+	export BOWTIE2_INDEXES
+
+	sr1=${sample_name}.${run_id}_1.${reference}.fasta.gz
+	sr1=${sample_name}.${run_id}_2.${reference}.fasta.gz
+	bowtie2 --very-sensitive-local --threads $threads -x $reference \
+		-q -1 $TMP/${run_id}_1.filt.fastq.gz -2 $TMP/${run_id}_2.filt.fastq.gz \
+		| gawk -F"\t" '
+#			BEGIN { OFS="\t" }
+			( /^@/ ){ print; next; }
+			( !and($2,4) || !and($2,8) ){
+#	Add aligned reference to read name? Or not?
+#				$1=$1 "_" $3
+				print }
+		' \
+		| samtools fasta -c 9 -1 $sr1 -2 $sr2 -
+#		| samtools fasta -c 9 -1 ${sample_name}.${run_id}_1.${reference}.fasta.gz -2 ${sample_name}.${run_id}_2.${reference}.fasta.gz -
+#		| samtools fastq -c 9 -1 ${sample_name}.${run_id}_1.${reference}.fastq.gz -2 ${sample_name}.${run_id}_2.${reference}.fastq.gz -
+
 #			FASTQ or FASTA?
 #
 #
 
 	#	 -s file True if file exists and has a size greater than zero.
-	[ -s ${sample}_1.${reference}.fastq.gz ] && aws s3 cp ${sample}_1.${reference}.fastq.gz ${S3}/
-	[ -s ${sample}_2.${reference}.fastq.gz ] && aws s3 cp ${sample}_2.${reference}.fastq.gz ${S3}/
+	[ -s $sr1 ] && aws s3 cp $sr1 ${S3}/
+	[ -s $sr2 ] && aws s3 cp $sr2 ${S3}/
 
+	if [ $do_aws_stuff == 'true' ] ; then
 
-	#	Apparently MUST be unmounted before will detach
-	sudo umount /dev/xvdf
+		#	Apparently MUST be unmounted before will detach
+		sudo umount /dev/xvdf
 
 	
-	command="aws ec2 detach-volume --region ${region} --volume-id ${volume_id}"
-	echo $command
-	response=$( $command )
-	echo $response
+		command="aws ec2 detach-volume --region ${region} --volume-id ${volume_id}"
+		echo $command
+		response=$( $command )
+		echo $response
 
-	state="in-use"
-	echo "Waiting for state to become 'available'"
-	until [ "$state" == "available" ] ; do
-		state=$( aws ec2 describe-volumes --region ${region} --volume-id ${volume_id} | jq '.Volumes[0].State' | tr -d '"' )
-	done
-
-
-#{
-#    "Volumes": [
-#        {
-#            "AvailabilityZone": "us-west-2c", 
-#            "Attachments": [
-#                {
-#                    "AttachTime": "2017-10-25T19:56:49.000Z", 
-#                    "InstanceId": "i-0905ad558e681257c", 
-#                    "VolumeId": "vol-00aa03a7f36fd9e9c", 
-#                    "State": "attached", 
-#                    "DeleteOnTermination": true, 
-#                    "Device": "/dev/xvda"
-#                }
-#            ], 
-#            "Encrypted": false, 
-#            "VolumeType": "gp2", 
-#            "VolumeId": "vol-00aa03a7f36fd9e9c", 
-#            "State": "in-use", 													<---- will be "available" when detached
-#            "Iops": 100, 
-#            "SnapshotId": "snap-23f09873", 
-#            "CreateTime": "2017-10-25T19:56:49.426Z", 
-#            "Size": 8
-#        }
-#    ]
-#}
+		state="in-use"
+		echo "Waiting for state to become 'available'"
+		until [ "$state" == "available" ] ; do
+			state=$( aws ec2 describe-volumes --region ${region} --volume-id ${volume_id} | jq '.Volumes[0].State' | tr -d '"' )
+		done
 
 
-	command="aws ec2 delete-volume --region ${region} --volume-id ${volume_id}"
-	echo $command
-	response=$( $command )
-	echo $response
+	#{
+	#    "Volumes": [
+	#        {
+	#            "AvailabilityZone": "us-west-2c",
+	#            "Attachments": [
+	#                {
+	#                    "AttachTime": "2017-10-25T19:56:49.000Z",
+	#                    "InstanceId": "i-0905ad558e681257c",
+	#                    "VolumeId": "vol-00aa03a7f36fd9e9c",
+	#                    "State": "attached",
+	#                    "DeleteOnTermination": true,
+	#                    "Device": "/dev/xvda"
+	#                }
+	#            ],
+	#            "Encrypted": false,
+	#            "VolumeType": "gp2",
+	#            "VolumeId": "vol-00aa03a7f36fd9e9c",
+	#            "State": "in-use", 													<---- will be "available" when detached
+	#            "Iops": 100,
+	#            "SnapshotId": "snap-23f09873",
+	#            "CreateTime": "2017-10-25T19:56:49.426Z",
+	#            "Size": 8
+	#        }
+	#    ]
+	#}
+
+
+		command="aws ec2 delete-volume --region ${region} --volume-id ${volume_id}"
+		echo $command
+		response=$( $command )
+		echo $response
+
+	fi
 
 	echo "Ending ..."
 	date
@@ -285,10 +301,10 @@ exit
 #	WITHDRAWN	WITHDRAWN_DATE	COMMENT	READ_COUNT	BASE_COUNT	ANALYSIS_GROUP
 
 #	data/HG03679/sequence_read/SRR824936_1.filt.fastq.gz
-#	
+#
 
 tail -n +2 ~/s3/1000genomes/sequence.index | \
-awk -F"\t" '( $20 ~ /2.filt.fastq.gz$/ && $26 != "exome" ){ print $10, $3 }' 
+awk -F"\t" '( $20 ~ /2.filt.fastq.gz$/ && $26 != "exome" ){ print $10, $3 }'
 
 #	-> 37065
 
