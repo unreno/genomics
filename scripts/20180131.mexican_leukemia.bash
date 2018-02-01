d0ba7241cb1ffcb665317fd23b070056832554d0
Jake <github@jakewendt.com>
2018-01-31 18:15:34 -0800

    Deving script to processing mexican leukemia study data.

--- scripts/20180131.mexican_leukemia.bash
+++ scripts/20180131.mexican_leukemia.bash
@@ -37,18 +37,29 @@
 bowtie2 --threads 4 --very-sensitive -x hg38 \
 	-1 24_S10_L001_R1_001.fastq.gz \
 	-2 24_S10_L001_R2_001.fastq.gz \
-	| samtools view --threads 3 -o 24_S10_L001.default.bam -
+	| samtools view --threads 3 -o 24_S10_L001.bam -
+chmod -w 24_S10_L001.bam
+
+#	This took a couple hours.
+
 
 #	Note, bowtie's threads is the actual count. Samtools is "in addition to 1". Stupid.
 
 #	Its gotta be sorted if selecting a region
 
-samtools sort --threads 3 -o 24_S10_L001.by_position.bam 24_S10_L001.default.bam
-samtools sort --threads 3 -n -o 24_S10_L001.by_name.bam 24_S10_L001.default.bam
+samtools sort --threads 3 -o 24_S10_L001.by_position.bam 24_S10_L001.bam
+chmod -w 24_S10_L001.by_position.bam
+
+samtools sort --threads 3 -n -o 24_S10_L001.by_name.bam 24_S10_L001.bam
+chmod -w 24_S10_L001.by_name.bam
+
+#	These sorts take only a few minutes.
 
 
-#	need an index to extract a region
+#	need an index to extract a region (here -@ is the actual number of threads)
+#samtools index -@ 4 24_S10_L001.by_position.bam
 samtools index 24_S10_L001.by_position.bam
+chmod -w 24_S10_L001.by_position.bam.bai
 
 #	hg38 gene
 #			samtools view -h -b -o "$bam_base.ERG.bam" "raw/$bam" "chr21:38367261-38662045"
@@ -56,15 +67,16 @@ samtools index 24_S10_L001.by_position.bam
 #			samtools view -h -b -o "$bam_base.ERG.bam" "raw/$bam" "chr21:38366261-38663045"
 
 samtools view -h -b -o 24_S10_L001.ERG.bam 24_S10_L001.by_position.bam "chr21:38366261-38663045"
+chmod -w 24_S10_L001.ERG.bam
 
 
 
 #	-F 2 = NOT PROPER_PAIR
-samtools fasta -F 2 --threads 3 -N -1 24_S10_L001.nonhg38.1.fasta -2 24_S10_L001.nonhg38.2.fasta
+#samtools fasta -F 2 --threads 3 -N -1 24_S10_L001.nonhg38.1.fasta -2 24_S10_L001.nonhg38.2.fasta 24_S10_L001.by_name.bam
+#cat 24_S10_L001.nonhg38.1.fasta 24_S10_L001.nonhg38.2.fasta | gzip --best > 24_S10_L001.nonhg38.fasta.gz
 
-cat 24_S10_L001.nonhg38.1.fasta 24_S10_L001.nonhg38.2.fasta > 24_S10_L001.nonhg38.fasta
-
-
+samtools fasta -F 2 --threads 3 -N 24_S10_L001.by_name.bam | gzip --best > 24_S10_L001.nonhg38.fasta.gz
+chmod -w 24_S10_L001.nonhg38.fasta.gz
 
 
 
@@ -89,7 +101,11 @@ cat 24_S10_L001.nonhg38.1.fasta 24_S10_L001.nonhg38.2.fasta > 24_S10_L001.nonhg3
 
 #	makeblastdb -dbtype nucl -in viral.fasta -out viral -title viral -parse_seqids
 
-blastn -query 24_S10_L001.nonhg38.fasta -db viral -num_threads 8 -out 24_S10_L001.nonhg38.blastn.txt
+#blastn -query <( zcat 24_S10_L001.nonhg38.fasta.gz ) -db viral -num_threads 8 -out 24_S10_L001.nonhg38.blastn.txt
+
+time blastn -query <( zcat 24_S10_L001.nonhg38.fasta.gz ) -db viral -num_threads 8 2> 24_S10_L001.nonhg38.blastn.STDERR.txt | gzip --best > 24_S10_L001.nonhg38.blastn.txt.gz
+
+#blastn -query 24_S10_L001.nonhg38.fasta -db viral -num_threads 8 -out 24_S10_L001.nonhg38.blastn.txt
 
 
 #				cmd="$prefix $command $negative_gilist -query $file -db $db $dust \
@@ -108,7 +124,11 @@ blastn -query 24_S10_L001.nonhg38.fasta -db viral -num_threads 8 -out 24_S10_L00
 
 #	https://github.com/bbuchfink/diamond/blob/master/diamond_manual.pdf
 
-diamond blastx --db ~/DIAMOND/viral --query 24_S10_L001.nonhg38.fasta --out 24_S10_L001.nonhg38.diamond.txt
+#diamond blastx --db ~/DIAMOND/viral --query 24_S10_L001.nonhg38.fasta --out 24_S10_L001.nonhg38.diamond.txt
+
+#diamond blastx --db ~/DIAMOND/viral --query <( zcat 24_S10_L001.nonhg38.fasta.gz ) --out 24_S10_L001.nonhg38.diamond.txt
+
+time diamond blastx --db ~/DIAMOND/viral --query <( zcat 24_S10_L001.nonhg38.fasta.gz ) 2> 24_S10_L001.nonhg38.diamond.STDERR.txt | gzip --best > 24_S10_L001.nonhg38.diamond.txt.gz
 
 
 
