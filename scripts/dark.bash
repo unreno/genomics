40badaeb18b91da7aa5dd0f761e28243e1327477
Jake <github@jakewendt.com>
2018-02-28 16:18:07 -0800

    Deving simple dark script.

--- scripts/dark.bash
+++ scripts/dark.bash
@@ -26,13 +26,17 @@ base=$1
 {
 	date
 
+	#	if the input are fastq, then --un, --al, etc are also. blastn only uses fastA
+	#	very fast, fast, sensitive, or very sensitive. Tried very fast, but probably should've gone the other way.
 	bowtie2 --threads ${threads} --very-fast -x hg38 \
 		-U $(ls ${base}*fastq.gz | paste -sd ',' ) \
-		--un-gz ${base}.nonhg38.fasta.gz \
-		-S /dev/null
+		| samtools fasta -f 4 --threads $[threads-1] -N -	\
+		| gzip --best > ${base}.nonhg38.fasta.gz
 	chmod -w ${base}.nonhg38.fasta.gz
 
-
+#		-S ${base}.sam
+	#chmod -w ${base}.sam
+#	rm ${base}.sam
 
 
 
@@ -59,14 +63,23 @@ base=$1
 	#	diamond makedb --in viral.protein.fasta --db viral
 
 	#	https://github.com/bbuchfink/diamond/blob/master/diamond_manual.pdf
+	#	--outfmt 0 is the same as blastn's default output format
 
 	diamond blastx \
+		--outfmt 0 \
 		--threads ${threads} \
 		--db ~/DIAMOND/viral \
 		--query <( zcat ${base}.nonhg38.fasta.gz ) \
 		2> /dev/null | gzip --best > ${base}.nonhg38.diamond.txt.gz
 
 	chmod -w ${base}.nonhg38.diamond.txt.gz
+	
+
 
 	date
 } > ${base}.log 2>&1
+chmod -w ${base}.log
+
+mkdir -p ../20180228.dark/
+mv ${base}.nonhg38.* ${base}.log ../20180228.dark/
+
