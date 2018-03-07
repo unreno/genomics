942e63aa82f967527d9e0239094f87d179bf7b8e
Jake <github@jakewendt.com>
2018-03-07 14:29:55 -0800

    Adding options to dark.

--- scripts/dark.bash
+++ scripts/dark.bash
@@ -3,14 +3,15 @@
 script=$( basename $0 )
 
 human="hg38"
-blastn_viral="/Users/jakewendt/BLAST_DBS/viral"
-diamond_viral="/Users/jakewendt/DIAMOND/viral"
+blastn_viral=""	#/Users/jakewendt/BLAST_DBS/viral"
+diamond_viral=""	#	/Users/jakewendt/DIAMOND/viral"
 threads=8
 
 function usage(){
 	echo
 	echo "Usage: ${script}"
 	echo
+	echo "$script [--human STRING] [--blastn_viral PATH] [--diamond_viral PATH] [--threads INTEGER] <file base(s)>"
 	echo
 	echo "Defaults:"
 	echo "	threads .......... ${threads}"
@@ -18,6 +19,9 @@ function usage(){
 	echo "	blastn_viral ..... ${blastn_viral}"
 	echo "	diamond_viral .... ${diamond_viral}"
 	echo
+	echo "Examples:"
+	echo
+	echo "$script --human hg38 --blastn_viral /Users/jakewendt/BLAST_DBS/viral --threads 8 H74_S6_L00"
 	echo
 	exit 1
 }
@@ -26,6 +30,14 @@ function usage(){
 
 while [ $# -ne 0 ] ; do
 	case $1 in
+		--threads)
+			shift; threads=$1; shift;;
+		--human)
+			shift; human=$1; shift;;
+		--blastn_viral)
+			shift; blastn_viral=$1; shift;;
+		--diamond_viral)
+			shift; diamond_viral=$1; shift;;
 #		-n|--estimated-num-cells)
 #			shift; num_cells=$1; shift;;
 #		-g|--genomedir)
@@ -91,13 +103,16 @@ while [ $# -ne 0 ] ; do
 
 		#	makeblastdb -dbtype nucl -in viral.fasta -out viral -title viral -parse_seqids
 
-		blastn -query <( zcat ${base}.non${human}.fasta.gz ) \
-			-db "${blastn_viral}" \
-			-num_threads ${threads} \
-			2> /dev/null | gzip --best > ${base}.non${human}.blastn.txt.gz
+		if [[ ! -z "${blastn_viral}" ]]; then
 
-		chmod -w ${base}.non${human}.blastn.txt.gz
+			blastn -query <( zcat ${base}.non${human}.fasta.gz ) \
+				-db "${blastn_viral}" \
+				-num_threads ${threads} \
+				2> /dev/null | gzip --best > ${base}.non${human}.blastn.txt.gz
 
+			chmod -w ${base}.non${human}.blastn.txt.gz
+
+		fi
 
 		#	ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/
 
@@ -109,14 +124,18 @@ while [ $# -ne 0 ] ; do
 		#	https://github.com/bbuchfink/diamond/blob/master/diamond_manual.pdf
 		#	--outfmt 0 is the same as blastn's default output format
 
-		diamond blastx \
-			--outfmt 0 \
-			--threads ${threads} \
-			--db "${diamond_viral}" \
-			--query <( zcat ${base}.non${human}.fasta.gz ) \
-			2> /dev/null | gzip --best > ${base}.non${human}.diamond.txt.gz
+		if [[ ! -z "${diamond_viral}" ]]; then
 
-		chmod -w ${base}.non${human}.diamond.txt.gz
+			diamond blastx \
+				--outfmt 0 \
+				--threads ${threads} \
+				--db "${diamond_viral}" \
+				--query <( zcat ${base}.non${human}.fasta.gz ) \
+				2> /dev/null | gzip --best > ${base}.non${human}.diamond.txt.gz
+
+			chmod -w ${base}.non${human}.diamond.txt.gz
+
+		fi
 
 		date
 	} > ${base}.log 2>&1
