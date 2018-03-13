3ee0b0f0ae490a8ae2a35e461e2308758331d6fd
Jake <github@jakewendt.com>
2018-03-13 15:48:17 -0700

    Trying with proper paired end-to-end alignment.

--- scripts/20180312.tcga_to_vcf.bash
+++ scripts/20180312.tcga_to_vcf.bash
@@ -23,11 +23,23 @@ fi
 #	Local seems to be the normal way to align before generating a VCF
 
 if [ ! -f ${base}.bam ] ; then
-	bowtie2 --threads 4 -x ${index} --very-sensitive-local -1 ${base}.1.fastq.gz -2 ${base}.2.fastq.gz -S ${base}.sam
-	samtools sort --threads 3 -o ${base}.bam ${base}.sam
+	#	local ALL alignments
+#	bowtie2 --threads 4 -x ${index} --very-sensitive-local -1 ${base}.1.fastq.gz -2 ${base}.2.fastq.gz -S ${base}.sam
+#	samtools sort --threads 3 -o ${base}.bam ${base}.sam
+
+
+	#	end-to-end only PROPER_PAIR alignments
+	#	This should actually find very little in our TCGA samples, since they've
+	#	already been filtered to include only HERV data.
+	bowtie2 --threads 4 -x ${index} --very-sensitive -1 ${base}.1.fastq.gz -2 ${base}.2.fastq.gz -S ${base}.sam
+	samtools view -f 2 -o ${base}.PP.bam ${base}.sam
+	samtools sort --threads 3 -o ${base}.bam ${base}.PP.bam
+#	\rm ${base}.PP.bam
+
+#	\rm ${base}.1.fastq.gz ${base}.2.fastq.gz
+#	\rm ${base}.sam
 fi
 
-#\rm ${base}.sam
 chmod -w ${base}.bam
 
 if [ ! -f ${base}.bam.bai ] ; then
@@ -35,47 +47,38 @@ if [ ! -f ${base}.bam.bai ] ; then
 fi
 chmod -w ${base}.bam.bai
 
-if [ ! -f ${base}.m.vcf.gz ] ; then
-	bcftools mpileup -Ou -f ${index}.fa ${base}.bam | bcftools call -mvO z -o ${base}.m.vcf.gz
-fi
-chmod -w ${base}.vcf.gz
-
-if [ ! -f ${base}.c.vcf.gz ] ; then
-	bcftools mpileup -Ou -f ${index}.fa ${base}.bam | bcftools call -cvO z -o ${base}.c.vcf.gz
-fi
-chmod -w ${base}.c.vcf.gz
 
 for o in 'c' 'm' ; do
+
+	if [ ! -f ${base}.${o}.vcf.gz ] ; then
+		#	u = uncompressed bcf
+		#	z =   compressed vcf
+		#	-c, --consensus-caller    : the original calling method (conflicts with -m)
+		#	-m, --multiallelic-caller : alternative model for multiallelic and rare-variant calling (conflicts with -c)
+		bcftools mpileup  --threads 1 --output-type u --fasta-ref ${index}.fa ${base}.bam \
+			| bcftools call --threads 1 --output-type z -${o} --variants-only --output ${base}.${o}.vcf.gz
+	fi
+	chmod -w ${base}.${o}.vcf.gz
+
 	for t in 'CT' 'GA'; do
 		from=${t: 0:1}
 		to=${t: -1:1}
 
+		#	collect chromosome and position
 		zcat ${base}.${o}.vcf.gz | awk -F"\t" '\
 			( !/^#/ && $4 == "'${from}'" && $5 == "'${to}'"){ \
-				print $1"\t"$2}' > ${base}.${o}.${t}.txt
+				print $1"\t"$2
+			}' > ${base}.${o}.${t}.txt
 
-
+		#	collect chromosome, position and the upcased trinucleotide centered about that position
 		#	using the while(){} loop in awk is to return the last line from samtools faidx
 		zcat ${base}.${o}.vcf.gz | awk -F"\t" '\
-		( !/^#/ && $4 == "'${from}'" && $5 == "'${to}'"){ \
-			while("samtools faidx '${index}'.fa "$1":"$2-1"-"$2+1" " | getline x ){}; \
-			print $1"\t"$2"\t"toupper(x) \
-		}' > ${base}.${o}.${t}.tri.txt
+			( !/^#/ && $4 == "'${from}'" && $5 == "'${to}'"){ \
+				while("samtools faidx '${index}'.fa "$1":"$2-1"-"$2+1" " | getline x ){}; \
+				print $1"\t"$2"\t"toupper(x) \
+			}' > ${base}.${o}.${t}.tri.txt
 
 	done	#	for t
+
 done	#	for o
-
-#zcat ${base}.m.vcf.gz | awk -F"\t" '\
-#( !/^#/ && $4 == "C" && $5 == "T"){ \
-#	while("samtools faidx '${index}'.fa "$1":"$2-1"-"$2+1" " | getline x ){}; \
-#	print $1"\t"$2"\t"toupper(x) \
-#}' > ${base}.m.CT.tri.txt
-#
-#zcat ${base}.m.vcf.gz | awk -F"\t" '\
-#( !/^#/ && $4 == "G" && $5 == "A"){ \
-#	while("samtools faidx '${index}'.fa "$1":"$2-1"-"$2+1" " | getline x ){}; \
-#	print $1"\t"$2"\t"toupper(x) \
-#}' > ${base}.m.GA.tri.txt
-
-
 
