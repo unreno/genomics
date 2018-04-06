a0c7bfdc959a019842f61308938aac66ed50cd11
Jake <github@jakewendt.com>
2018-04-05 20:16:18 -0700

    Deving vcf analysis script.

---
+++ scripts/20180405.1000genomes.vcf.bash
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+index=~/s3/herv/indexes/build37
+
+for vcf in $( find ~/s3/1000genomes/phase3/ -name \*vcf.gz ) ; do
+	echo $vcf
+
+#/Users/jakewendt/s3/1000genomes/phase3//data/HG00477/cg_data/HG00477_lcl_SRR826683.wgs.COMPLETE_GENOMICS.20130401.snps_indels_svs_meis.high_coverage.genotypes.vcf.gz
+
+
+	base=$( basename $vcf )
+#	base=${base%%_*}
+	base=${base%%.*}
+	echo $base
+
+#	for t in 'CT' 'GA'; do
+#		from=${t: 0:1}	#	first letter
+#		to=${t: -1:1}		#	last letter
+#
+#		#	collect chromosome and position
+#		zcat $vcf | awk -F"\t" '\
+#			( !/^#/ && $4 == "'${from}'" && $5 == "'${to}'"){ \
+#				print $1"\t"$2
+#			}' > ${base}.${o}.${t}.txt
+#
+#		#	collect chromosome, position and the upcased trinucleotide centered about that position
+#		#	using the while(){} loop in awk is to return the last line from samtools faidx
+#		zcat $vcf | awk -F"\t" '\
+#			( !/^#/ && $4 == "'${from}'" && $5 == "'${to}'"){ \
+#				while("samtools faidx '${index}'.fa "$1":"$2-1"-"$2+1" " | getline x ){}; \
+#				print $1"\t"$2"\t"toupper(x) \
+#			}' > ${base}.${o}.${t}.tri.txt
+#
+#	done	#	for t
+
+
+#	My build37 contains >chr1
+#	So does the referenced version?
+#	ftp://ftp.completegenomics.com/ReferenceFiles/build37.fa.bz2
+#	1000genomes vcfs (referenced to build37) contains just the number so add "chr"
+
+	if [ ! -f ${base}.txt ] ; then
+
+		#	This version takes only a few seconds per file
+		zcat $vcf | awk -F"\t" '\
+			( !/^#/ && ( ( $4 == "C" && $5 == "T" ) || ( $4 == "G" && $5 == "A" ) ) ){ \
+				print $1"\t"$2"\t"$4"\t"$5
+			}' > ${base}.txt
+
+	fi
+
+	#	There are limits as to the number of open files or pipes.
+	#	the samtools call from within awk MUST be closed otherwise errors like ...
+	#		awk: cmd. line:3: (FILENAME=- FNR=289293) fatal: cannot open pipe `samtools faidx /Users/jakewendt/s3/herv/indexes/build37.fa chr1:79714090-79714092 ' (Too many open files)
+
+	if [ ! -f ${base}.tri.txt ] ; then
+
+#				while("samtools faidx '${index}'.fa chr"$1":"$2-1"-"$2+1" " | getline x ){}; \
+		#	This version takes a few minutes per file
+		zcat $vcf | awk -F"\t" '\
+			( !/^#/ && ( ( $4 == "C" && $5 == "T" ) || ( $4 == "G" && $5 == "A" ) ) ){ \
+				samtools = "samtools faidx '${index}'.fa chr"$1":"$2-1"-"$2+1" "
+				while(samtools | getline x ){}; \
+				close(samtools);
+				print $1"\t"$2"\t"$4"\t"$5"\t"toupper(x) \
+			}' > ${base}.tri.txt
+
+	fi
+
+done
