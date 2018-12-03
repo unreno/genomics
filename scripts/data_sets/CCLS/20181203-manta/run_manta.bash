83bc1a3c42166f9d0d6b490534f9d66c87c5f133
Jake <github@jakewendt.com>
2018-12-03 12:45:53 -0700

    Prepping for re-run

--- scripts/data_sets/CCLS/20181203-manta/run_manta.bash
+++ scripts/data_sets/CCLS/20181203-manta/run_manta.bash
@@ -6,13 +6,24 @@
 for base in 983899 ; do
  
 	configManta.py \
-		--normalBam /raid/data/raw/CCLS/bam/redo/GM_${base}.bam \
-		--tumorBam /raid/data/raw/CCLS/bam/redo/${base}.bam \
-		--referenceFasta /raid/refs/fasta/hg38.num.fa \
+		--normalBam /raid/data/raw/CCLS/bam/redo/GM_${base}.hg38_no_alts.RG.bam \
+		--tumorBam /raid/data/raw/CCLS/bam/redo/${base}.hg38_no_alts.RG.bam \
+		--referenceFasta /raid/refs/fasta/hg38_no_alts.fa \
 		--runDir ${base}
 
 	#Seems that each option NEEDS an explicit path
 
 	${base}/runWorkflow.py -m local -j 40
+
+
+	configManta.py \
+		--normalBam /raid/data/raw/CCLS/bam/redo/GM_${base}.hg38_no_alts.PP.RG.bam \
+		--tumorBam /raid/data/raw/CCLS/bam/redo/${base}.hg38_no_alts.PP.RG.bam \
+		--referenceFasta /raid/refs/fasta/hg38_no_alts.fa \
+		--runDir ${base}.PP
+
+	#Seems that each option NEEDS an explicit path
+
+	${base}.PP/runWorkflow.py -m local -j 40
 
 done
--- scripts/data_sets/CCLS/20181203-strelka/run_strelka.bash
+++ scripts/data_sets/CCLS/20181203-strelka/run_strelka.bash
@@ -6,17 +6,36 @@
 #/raid/data/raw/20180718-Adam/bam/GM_634370.recaled.bam GM_634370
 #/raid/data/raw/20180718-Adam/bam/GM_983899.recaled.bam GM_983899
 
+#	/raid/data/raw/CCLS/bam/redo/GM_983899.hg38_no_alts.PP.RG.bam
+
+
+#	For consistency, I should have use hg38.num.fa instead of hg38_no_alts.fa
+#	HOWEVER, the latest dbsnp reference uses the "chr" prefix. Would need to edit.
+
+
 #for base in 268325 439338 63185 634370 983899 ; do
 for base in 983899 ; do
 
 	configureStrelkaSomaticWorkflow.py \
-		--normalBam /raid/data/raw/CCLS/bam/redo/GM_${base}.bam \
-		--tumorBam /raid/data/raw/CCLS/bam/redo/${base}.bam \
-		--ref /raid/refs/fasta/hg38.num.fa \
+		--normalBam /raid/data/raw/CCLS/bam/redo/GM_${base}.hg38_no_alts.RG.bam \
+		--tumorBam /raid/data/raw/CCLS/bam/redo/${base}.hg38_no_alts.RG.bam \
+		--ref /raid/refs/fasta/hg38_no_alts.fa \
 		--runDir ${base}
 
 	#Seems that each option NEEDS an explicit path
 
 	${base}/runWorkflow.py -m local -j 40
+
+
+
+	configureStrelkaSomaticWorkflow.py \
+		--normalBam /raid/data/raw/CCLS/bam/redo/GM_${base}.hg38_no_alts.PP.RG.bam \
+		--tumorBam /raid/data/raw/CCLS/bam/redo/${base}.hg38_no_alts.PP.RG.bam \
+		--ref /raid/refs/fasta/hg38_no_alts.fa \
+		--runDir ${base}.PP
+
+	#Seems that each option NEEDS an explicit path
+
+	${base}.PP/runWorkflow.py -m local -j 40
 
 done
