a7b6b8dda7a7a0b05318a444a0162eda43cb3b9a
Jake <github@jakewendt.com>
2018-12-07 17:33:22 -0700

    Creating VCFs

--- scripts/data_sets/CCLS/raw/bam/redo/create_vcfs.bash
+++ scripts/data_sets/CCLS/raw/bam/redo/create_vcfs.bash
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 
-gatk HaplotypeCaller --input GM_983899.hg38.num.bam --output GM_983899.hg38.num.vcf.gz --reference /raid/refs/fasta/hg38.num.fa --dbsnp /raid/refs/vcf/dbsnp_146.hg38.num.MT.vcf.gz > GM_983899.hg38.num.vcf.gz.out 2> GM_983899.hg38.num.vcf.gz.err
+gatk HaplotypeCaller --input GM_983899.hg38.num.bam --output GM_983899.hg38.num.vcf.gz --reference /raid/refs/fasta/hg38.num.fa --dbsnp /raid/refs/vcf/dbsnp_146.hg38.num.MT.vcf.gz > GM_983899.hg38.num.vcf.gz.out 2> GM_983899.hg38.num.vcf.gz.err &
 
-gatk HaplotypeCaller --input 983899.hg38.num.bam --output 983899.hg38.num.vcf.gz --reference /raid/refs/fasta/hg38.num.fa --dbsnp /raid/refs/vcf/dbsnp_146.hg38.num.MT.vcf.gz > 983899.hg38.num.vcf.gz.out 2> 983899.hg38.num.vcf.gz.err
+gatk HaplotypeCaller --input 983899.hg38.num.bam --output 983899.hg38.num.vcf.gz --reference /raid/refs/fasta/hg38.num.fa --dbsnp /raid/refs/vcf/dbsnp_146.hg38.num.MT.vcf.gz > 983899.hg38.num.vcf.gz.out 2> 983899.hg38.num.vcf.gz.err &
 
-gatk HaplotypeCaller --input GM_983899.hg38.num.PP.bam --output GM_983899.hg38.num.PP.vcf.gz --reference /raid/refs/fasta/hg38.num.fa --dbsnp /raid/refs/vcf/dbsnp_146.hg38.num.MT.vcf.gz > GM_983899.hg38.num.PP.vcf.gz.out 2> GM_983899.hg38.num.PP.vcf.gz.err
+gatk HaplotypeCaller --input GM_983899.hg38.num.PP.bam --output GM_983899.hg38.num.PP.vcf.gz --reference /raid/refs/fasta/hg38.num.fa --dbsnp /raid/refs/vcf/dbsnp_146.hg38.num.MT.vcf.gz > GM_983899.hg38.num.PP.vcf.gz.out 2> GM_983899.hg38.num.PP.vcf.gz.err &
 
-gatk HaplotypeCaller --input 983899.hg38.num.PP.bam --output 983899.hg38.num.PP.vcf.gz --reference /raid/refs/fasta/hg38.num.fa --dbsnp /raid/refs/vcf/dbsnp_146.hg38.num.MT.vcf.gz > 983899.hg38.num.PP.vcf.gz.out 2> 983899.hg38.num.PP.vcf.gz.err
+gatk HaplotypeCaller --input 983899.hg38.num.PP.bam --output 983899.hg38.num.PP.vcf.gz --reference /raid/refs/fasta/hg38.num.fa --dbsnp /raid/refs/vcf/dbsnp_146.hg38.num.MT.vcf.gz > 983899.hg38.num.PP.vcf.gz.out 2> 983899.hg38.num.PP.vcf.gz.err &
 
 
