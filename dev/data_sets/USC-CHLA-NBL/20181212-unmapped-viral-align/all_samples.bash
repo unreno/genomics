b764b4fb190eba49e807d4b6e0329cbe27ba5bc3
Jake <github@jakewendt.com>
2018-12-12 17:36:55 -0700

    Deving new project

--- dev/data_sets/USC-CHLA-NBL/20181212-unmapped-viral-align/all_samples.bash
+++ dev/data_sets/USC-CHLA-NBL/20181212-unmapped-viral-align/all_samples.bash
@@ -7,7 +7,7 @@
 for virus in NC_001664.4 NC_000898.1 NC_08168.1 ; do
 	echo Align to $virus
 	bowtie2 --threads 35 --xeq -x virii/${virus} --very-sensitive -1 $( ls /raid/data/raw/USC-CHLA-NBL/2018????/*.R1.fastq.gz | paste -sd ',' ) -2 $( ls /raid/data/raw/USC-CHLA-NBL/2018????/*.R2.fastq.gz | paste -sd ',' ) 2>> ${virus}.log | samtools view -F 4 -o ${virus}.unsorted.bam -
-	chmod a-w ${virus}.unsorted.bam
+	#chmod a-w ${virus}.unsorted.bam
 	echo Sort
 	samtools sort -o ${virus}.bam ${virus}.unsorted.bam
 	chmod a-w ${virus}.bam
