2cd39c768521fd904292f1aec91ded60f73b028d
Jake <github@jakewendt.com>
2018-12-17 21:53:14 -0800

    Meddling

--- dev/data_sets/CCLS/raw/bam/bam-coverage-depth.py
+++ dev/data_sets/CCLS/raw/bam/bam-coverage-depth.py
@@ -6,25 +6,28 @@ import glob
 import os.path
 
 from matplotlib.backends.backend_pdf import PdfPages
-pp = PdfPages('20181217-Bam-coverage-depth.pdf')
+pp = PdfPages('bam-coverage-depth.pdf')
 
-filename="/Users/jakewendt/github/jakewendt/notebooks/983899.recaled.bam.depth.txt.gz"
+filename="/raid/data/raw/CCLS/bam/983899.recaled.bam.depth.txt.gz"
 
-#pickle="/Users/jakewendt/github/jakewendt/notebooks/983899.recaled.bam.depth.txt.gz"
-#d = pandas.read_pickle("/Users/jakewendt/github/jakewendt/notebooks/983899.recaled.bam.depth.txt.pkl")
+pickle_filename="/raid/data/raw/CCLS/bam/983899.recaled.bam.depth.txt.gz"
+#d = pandas.read_pickle(pickle_filename)
 
 #print(filename)
 if os.path.isfile(filename): 
+	print("Reading "+filename)
 	d = pandas.read_csv(filename,
 		sep="\t",
 		header=None,
 		usecols=[0,1,2],
 		names=["chromosome","position","depth"],
-		dtype={ "chromosome": str }
+		dtype={ "chromosome": str },
 		index_col=["chromosome","position"] )
 
-	d.to_pickle("/Users/jakewendt/github/jakewendt/notebooks/983899.recaled.bam.depth.txt.pkl")
+	print("Writing "+pickle_filename)
+	d.to_pickle(pickle_filename)
 
+	print("Plotting")
 	for idx, data in d.groupby(level=0):
 		print('---')
 		print(idx)
