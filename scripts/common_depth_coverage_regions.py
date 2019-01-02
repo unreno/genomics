87d97bfca298ac3ec940c30b8541cc1ea4fdad7f
Jake <github@jakewendt.com>
2019-01-02 10:35:54 -0800

    Deving new script to extract common regions from collection of depth files.

--- scripts/common_depth_coverage_regions.py
+++ scripts/common_depth_coverage_regions.py
@@ -5,12 +5,11 @@ import os
 #import os.path
 import sys
 import pandas
+import numpy
 
 
 #	common_depth_coverage_regions.py {HG,NA}*.NC_001664.4.depth.csv
 #	common_depth_coverage_regions.py {HG,NA}*.NC_000898.1.depth.csv
-
-
 
 
 #print( sys.argv[1:] )
@@ -20,21 +19,89 @@ import pandas
 #20180103-awsqueue.sql
 
 
-
 #	~/github/unreno/genomics/dev/data_sets/1000genomes/20181010-unmapped-hhv6/hhv6.py
 #	dev/data_sets/CCLS/raw/bam/bam-coverage-depth.py
 
+#	pvalues = pandas.DataFrame(index=trinuc_muts,columns=trinuc_muts)
+#d=pandas.DataFrame(columns=['position','depth'])
+#d.set_index('position',inplace=True)
+#d=pandas.DataFrame(columns=['position','depth'])
+#df=pandas.DataFrame(data=None, index=['position'], columns=['depth'], dtype=int)
+#df=pandas.DataFrame(data=None, index=['position'], columns=[], dtype=int)
+
+df=pandas.DataFrame()
+boo=pandas.DataFrame()
+flagged=pandas.DataFrame()
+print( df.head() )
+print( df.dtypes )
 
 for filename in sys.argv[1:]:
 	print(filename)
 	if os.path.isfile(filename) and os.path.getsize(filename) > 0:
 		print("Reading "+filename)
+		sample=filename.split(".")[0]	#	everything before the first "."
 		d = pandas.read_csv(filename,
 			sep="\t",
 			header=None,
-			usecols=[0,1],
-			names=["position","depth"],
+			usecols=[1,2],	# usecols=[0,1],
+			#names=["position","depth"],
+			#dtype={"position": int,"depth": int},
+			names=["position",sample],
+			dtype={"position": int, sample: int},
 			index_col=["position"] )
+		print( d.head() )
+		print( d.dtypes )
+		#df = df.merge( d )
+		#df = pandas.concat([df, d], axis=1, sort=True )
+		df = pandas.concat([df, d], axis=1)
+		#.fillna(0)
+		print( df.head() )
+		print( df.dtypes )
+		#d[sample]=d[sample]>100
+		#d[sample] = d.apply(lambda row: row[sample] > 100 , axis=1)
+		#boo = pandas.concat([boo, d], axis=1)
+		#print( boo.head() )
+		#print( boo.dtypes )
+		#	NOPE d[sample]=( d[sample]>1 ) ? 1 : 0
+
+		#print( "SUM" )
+		#print( d.sum() )
+		d[sample] = numpy.where(d[sample]>1, 1, 0)
+
+		flagged = pandas.concat([flagged, d], axis=1)
+		print( flagged.head() )
+		print( flagged.dtypes )
+
+		#	merge
+#subjects = subjects.merge( kg, left_on='Subject', right_on='subject', how='outer')
+
+df.fillna(0, inplace=True)
+print( df.head() )
+print( df.tail() )
+print( df.dtypes )
+print( df )
+#print( df.mean(axis=1) )
+
+#boo.fillna(False, inplace=True)
+#print( boo.head() )
+#print( boo.dtypes )
+#print( boo.mean(axis=1).tail() )
 
 
+flagged.fillna(0, inplace=True)
+print( flagged.head() )
+print( flagged.dtypes )
+print( flagged.mean(axis=1) )
+
+
+common=flagged.index[ flagged.mean(axis=1) > 0.6 ].tolist()
+print( common )
+
+last=0
+
+for i in common:
+	if( i != last+1 ):
+		print("Region end: ",last)
+		print("Region start: ",i)
+	last=i
 
