39856fae4c2a10b9d5ce99f451676d9658ded898
Jake <github@jakewendt.com>
2019-01-07 17:00:54 -0800

    Deving more unmapped analysis.

--- dev/data_sets/1000genomes/20181214-unmapped-virii/process.bash
+++ dev/data_sets/1000genomes/20181214-unmapped-virii/process.bash
@@ -6,7 +6,31 @@ set -u	#	Error on usage of unset variables
 set -o pipefail
 
 
-#for bam in $( find /raid/data/raw/1000genomes/phase3/data/ -name *unmapped*bam ) ; do
+
+p=2 #	percent threshold use to call common coverage
+
+database_file="viral_mapped_unmapped.2000.${p}.sqlite3"
+
+sql="sqlite3 ${database_file}"
+
+if [ ! -f ${database_file} ] ; then
+
+	${sql} "CREATE TABLE subjects( subject, unmapped, mapped, total ); CREATE UNIQUE INDEX subjects_subject ON subjects (subject);"
+
+	for virus in /raid/refs/fasta/virii/*fasta ; do
+		virus=$( basename $virus .fasta )
+		virus=${virus/./_}
+		${sql} "ALTER TABLE subjects ADD COLUMN ${virus}"
+		${sql} "ALTER TABLE subjects ADD COLUMN ${virus}_unmapped"
+		${sql} "ALTER TABLE subjects ADD COLUMN ${virus}_total"
+		${sql} "ALTER TABLE subjects ADD COLUMN uncommon_${virus}"
+		${sql} "ALTER TABLE subjects ADD COLUMN uncommon_${virus}_unmapped"
+		${sql} "ALTER TABLE subjects ADD COLUMN uncommon_${virus}_total"
+	done
+
+fi
+
+
 for bam in /raid/data/raw/1000genomes/phase3/data/*/alignment/*unmapped*bam ; do
 	echo "------------------------------------------------------------"
 	base=$( basename $bam )
@@ -17,212 +41,229 @@ for bam in /raid/data/raw/1000genomes/phase3/data/*/alignment/*unmapped*bam ; do
 
 	echo "Processing $subject"
 
-
-	if [ -f unmapped.count.txt/${subject}.unmapped.count.txt ] && [ ! -w unmapped.count.txt/${subject}.unmapped.count.txt ]  ; then
-		echo "Write-protected ${subject}.unmapped.count.txt exists. Skipping step."
+	f="${subject}/${subject}.unmapped.count.txt"
+	if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+		echo "Write-protected ${f} exists. Skipping step."
 	else
 		echo "Counting reads in $bam.bai"
 		#samtools view -c $bam > ${subject}.unmapped.count.txt
-		cat $bam.bai | bamReadDepther | grep "^*\|^#" | awk -F"\t" '{s+=$NF}END{print s}' > unmapped.count.txt/${subject}.unmapped.count.txt
-		chmod a-w unmapped.count.txt/${subject}.unmapped.count.txt
+		cat $bam.bai | bamReadDepther | grep "^*\|^#" | awk -F"\t" '{s+=$NF}END{print s}' > ${f}
+		chmod a-w ${f}
 	fi
 
-	if [ -f mapped.count.txt/${subject}.mapped.count.txt ] && [ ! -w mapped.count.txt/${subject}.mapped.count.txt ]  ; then
-		echo "Write-protected ${subject}.mapped.count.txt exists. Skipping step."
+	f="${subject}/${subject}.mapped.count.txt"
+	if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+		echo "Write-protected ${f} exists. Skipping step."
 	else
 		mapped_bam=${bam/.unmapped/.mapped}
 		echo "Counting reads in $mapped_bam.bai"
 		#	DIFFERENT THAN UNMAPPED
-		cat $mapped_bam.bai | bamReadDepther | grep "^#" | awk -F"\t" '{s+=$2+$3}END{print s}' > mapped.count.txt/${subject}.mapped.count.txt
-		chmod a-w mapped.count.txt/${subject}.mapped.count.txt
+		cat $mapped_bam.bai | bamReadDepther | grep "^#" | awk -F"\t" '{s+=$2+$3}END{print s}' > ${f}
+		chmod a-w ${f}
 	fi
 
-	if [ -f total.count.txt/${subject}.total.count.txt ] && [ ! -w total.count.txt/${subject}.total.count.txt ]  ; then
-		echo "Write-protected ${subject}.total.count.txt exists. Skipping step."
+	f="${subject}/${subject}.total.count.txt"
+	if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+		echo "Write-protected ${f} exists. Skipping step."
 	else
 		echo "Adding mapped and unmapped read counts."
-		echo $( cat mapped.count.txt/${subject}.mapped.count.txt )"+"$( cat ${subject}.unmapped.count.txt ) | bc > total.count.txt/${subject}.total.count.txt
-		chmod a-w total.count.txt/${subject}.total.count.txt
+		echo $( cat ${subject}/${subject}.mapped.count.txt )"+"$( cat ${subject}/${subject}.unmapped.count.txt ) | bc > ${f}
+		chmod a-w ${f}
 	fi
 
-#	if [ -f fastq.gz/${subject}.fastq.gz ] && [ ! -w fastq.gz/${subject}.fastq.gz ]  ; then
-#		echo "Write-protected ${subject}.fastq.gz exists. Skipping step."
-#	else
-#		echo "Extracting fastq reads from $bam"
-#		samtools fastq -N $bam | gzip --best > fastq.gz/${subject}.fastq.gz
-#		chmod a-w fastq.gz/${subject}.fastq.gz
-#	fi
+	if [ -z $( ${sql} "SELECT unmapped FROM subjects WHERE subject = '${subject}'" ) ] ; then
+		mapped=$( cat ${subject}/${subject}.mapped.count.txt )
+		unmapped=$( cat ${subject}/${subject}.unmapped.count.txt )
+		total=$( cat ${subject}/${subject}.total.count.txt )
+		command="INSERT INTO subjects( subject, unmapped, mapped, total ) VALUES ( '${subject}', '${unmapped}', '${mapped}', '${total}' );"
+		echo "${command}"
+		${sql} "${command}"
+	fi
 
-#	blastn only accepts fasta. Bowtie2 can take either. Not sure if bowtie2 uses the quality
 
-	if [ -f ${subject}.fasta.gz ] && [ ! -w ${subject}.fasta.gz ]  ; then
-		echo "Write-protected ${subject}.fasta.gz exists. Skipping step."
+	#	blastn only accepts fasta. Bowtie2 can take either. Not sure if bowtie2 uses the quality
+
+	f="${subject}.fasta.gz"
+	if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+		echo "Write-protected ${f} exists. Skipping step."
 	else
 		echo "Extracting fasta reads from $bam"
-		samtools fasta -N $bam 2>> ${subject}.fasta.log | gzip --best > ${subject}.fasta.gz
-		chmod a-w ${subject}.fasta.gz
+		samtools fasta -N $bam 2>> ${subject}/${subject}.fasta.log | gzip --best > ${f}
+		chmod a-w ${f}
 	fi
 
-
-
-	if [ -f virii.bam/${subject}.virii.bam ] && [ ! -w virii.bam/${subject}.virii.bam ]  ; then
-		echo "Write-protected ${subject}.virii.bam exists. Skipping step."
+	f="${subject}/${subject}.virii.bam"
+	if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+		echo "Write-protected ${f} exists. Skipping step."
 	else
 
-		if [ -f ${subject}.virii.unsorted.bam ] && [ ! -w ${subject}.virii.unsorted.bam ]  ; then
-			echo "Write-protected ${subject}.virii.unsorted.bam exists. Skipping step."
+		f="${subject}/${subject}.virii.sorted.bam"
+		if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+			echo "Write-protected ${f} exists. Skipping step."
 		else
 			echo "Aligning ${subject} to all virii."
 
 
 #	Hmm. All?
 
-			bowtie2 --all --threads 35 -f --xeq -x virii --very-sensitive -U ${subject}.fasta.gz 2>> ${subject}.virii.log | samtools view -F 4 -o ${subject}.virii.unsorted.bam -
+			bowtie2 --all --threads 35 -f --xeq -x virii --very-sensitive -U ${subject}/${subject}.fasta.gz 2>> ${subject}/${subject}.virii.log | samtools view -F 4 -o ${f} -
 
 
 			#chmod a-w ${subject}.virii.unsorted.bam
 		fi
 
 		echo "Sorting"
-		samtools sort -o virii.bam/${subject}.virii.bam ${subject}.virii.unsorted.bam
-		chmod a-w virii.bam/${subject}.virii.bam
+		samtools sort -o ${subject}/${subject}.virii.bam ${f}
+		chmod a-w ${subject}/${subject}.virii.bam
 
-		\rm ${subject}.virii.unsorted.bam
+		\rm ${f}
 	fi
 
-	if [ -f virii.bam/${subject}.virii.bam.bai ] && [ ! -w virii.bam/${subject}.virii.bam.bai ]  ; then
-		echo "Write-protected ${subject}.virii.bam.bai exists. Skipping step."
+	f="${subject}/${subject}.virii.bam.bai"
+	if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+		echo "Write-protected ${f} exists. Skipping step."
 	else
 		echo "Indexing"
-		samtools index virii.bam/${subject}.virii.bam
-		chmod a-w virii.bam/${subject}.virii.bam.bai
+		samtools index ${subject}/${subject}.virii.bam
+		chmod a-w ${f}
 	fi
 
-	if [ -f virii.depth.csv/${subject}.virii.depth.csv ] && [ ! -w virii.depth.csv/${subject}.virii.depth.csv ]  ; then
-		echo "Write-protected ${subject}.virii.depth.csv exists. Skipping step."
+	f="${subject}/${subject}.virii.depth.csv"
+	if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+		echo "Write-protected ${f} exists. Skipping step."
 	else
 		echo "Getting depth"
-		samtools depth virii.bam/${subject}.virii.bam > virii.depth.csv/${subject}.virii.depth.csv
-		chmod a-w virii.depth.csv/${subject}.virii.depth.csv
+		samtools depth ${subject}/${subject}.virii.bam > ${f}
+		chmod a-w ${f}
 	fi
 
 
 	for virus in /raid/refs/fasta/virii/*fasta ; do
 		virus=$( basename $virus .fasta )
+		v=${virus/./_}
 
 		echo "Processing $subject / $virus"
 
-		if [ -f depth.csv/${subject}.${virus}.depth.csv ] && [ ! -w depth.csv/${subject}.${virus}.depth.csv ]  ; then
-			echo "Write-protected ${subject}.${virus}.depth.csv exists. Skipping step."
+		f="${subject}/${subject}.${virus}.depth.csv"
+		if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+			echo "Write-protected ${f} exists. Skipping step."
 		else
 			echo "Getting depth"
 
 
-			awk '( $1 == "'${virus}'" )' virii.depth.csv/${subject}.virii.depth.csv > depth.csv/${subject}.${virus}.depth.csv
+			awk '( $1 == "'${virus}'" )' ${subject}/${subject}.virii.depth.csv > ${f}
 #			samtools depth ${subject}.${virus}.bam > ${subject}.${virus}.depth.csv
 
 
 
-			chmod a-w depth.csv/${subject}.${virus}.depth.csv
+			chmod a-w ${f}
 		fi
 
 
-		if [ -f bowtie2.mapped.count.txt/${subject}.${virus}.bowtie2.mapped.count.txt ] && [ ! -w bowtie2.mapped.count.txt/${subject}.${virus}.bowtie2.mapped.count.txt ]  ; then
-			echo "Write-protected ${subject}.${virus}.bowtie2.mapped.count.txt exists. Skipping step."
+		f="${subject}/${subject}.${virus}.bowtie2.mapped.count.txt"
+		if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+			echo "Write-protected ${f} exists. Skipping step."
 		else
 			echo "Counting reads bowtie2 aligned to ${virus}"
 			#	-F 4 needless here as filtered with this flag above.
-			samtools view -c -F 4 virii.bam/${subject}.virii.bam ${virus} > bowtie2.mapped.count.txt/${subject}.${virus}.bowtie2.mapped.count.txt
-			chmod a-w bowtie2.mapped.count.txt/${subject}.${virus}.bowtie2.mapped.count.txt
+			samtools view -c -F 4 ${subject}/${subject}.virii.bam ${virus} > ${f}
+			chmod a-w ${f}
+		fi
+
+		if [ -z $( ${sql} "SELECT ${v} FROM subjects WHERE subject = '${subject}'" ) ] ; then
+			count=$( cat ${subject}/${subject}.${virus}.bowtie2.mapped.count.txt )
+			command="UPDATE subjects SET ${v} = '${count}' WHERE subject = '${subject}'"
+			echo "${command}"
+			${sql} "${command}"
 		fi
 
 
 
 
 
+		for g in 2000 ; do
 
-
-#		if [ -f bowtie2.mapped_uncommon.50.count.txt/${subject}.${virus}.bowtie2.mapped_uncommon.50.count.txt ] && [ ! -w bowtie2.mapped_uncommon.50.count.txt/${subject}.${virus}.bowtie2.mapped_uncommon.50.count.txt ]  ; then
-#			echo "Write-protected ${subject}.${virus}.bowtie2.mapped_uncommon.50.count.txt exists. Skipping step."
-#		else
-#			echo "Counting reads bowtie2 aligned uncommon.50 to ${virus}"
-#			#	-F 4 needless here as filtered with this flag above.
-#
-#			region=$( grep Samtools common_regions.50.${virus}.txt || true ) #	grep will return error code if no line found so add || true
-#			echo $region
-#			region=${region#Samtools uncommon regions: }
-#			#common_regions.D13784.1.txt:Samtools uncommon regions: D13784.1:1-4163 D13784.1:4208-7649 D13784.1:7691-8000 D13784.1:8053-1000000
-#
-#			echo "${region}"
-#			[ -z "${region}" ] && region="${virus}"
-#
-#			samtools view -c -F 4 virii.bam/${subject}.virii.bam ${region} > bowtie2.mapped_uncommon.50.count.txt/${subject}.${virus}.bowtie2.mapped_uncommon.50.count.txt
-#
-#
-#			chmod a-w bowtie2.mapped_uncommon.50.count.txt/${subject}.${virus}.bowtie2.mapped_uncommon.50.count.txt
-#		fi
-
-
-
-
-#		for p in 50 25 5 ; do
-#		for p in 5 ; do
-		for p in 2 ; do
-
-			outdir="bowtie2.mapped_uncommon.2000.${p}.count.txt"
-			outfile="${subject}.${virus}.bowtie2.mapped_uncommon.${p}.count.txt"
-
-			mkdir -p "${outdir}/"
-
-			if [ -f ${outdir}/${outfile} ] && [ ! -w ${outdir}/${outfile} ]  ; then
-				echo "Write-protected ${outfile} exists. Skipping step."
-			else
-				echo "Counting reads bowtie2 aligned uncommon.${p} to ${virus}"
-				#	-F 4 needless here as filtered with this flag above.
+			#		for p in 50 25 5 ; do
+			#		for p in 5 ; do
+			for p in 2 ; do
 	
-				region=$( grep Samtools common_regions.2000.${p}.${virus}.txt || true ) #	grep will return error code if no line found so add || true
-				echo $region
-				region=${region#Samtools uncommon regions: }
-				#common_regions.D13784.1.txt:Samtools uncommon regions: D13784.1:1-4163 D13784.1:4208-7649 D13784.1:7691-8000 D13784.1:8053-1000000
-	
-				echo "${region}"
-				[ -z "${region}" ] && region="${virus}"
-	
-				samtools view -c -F 4 virii.bam/${subject}.virii.bam ${region} > ${outdir}/${outfile}
-	
-				chmod a-w ${outdir}/${outfile}
-			fi
+				if [ -f common_regions/common_regions.${g}.${p}.${virus}.txt ] ; then
+
+					f="${subject}/${subject}.${virus}.bowtie2.mapped_uncommon.${g}.${p}.count.txt"
+		
+					if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+						echo "Write-protected ${f} exists. Skipping step."
+					else
+						echo "Counting reads bowtie2 aligned uncommon.${g}.${p} to ${virus}"
+						#	-F 4 needless here as filtered with this flag above.
+			
+						#	grep will return error code if no line found so add || true
+						region=$( grep Samtools common_regions/common_regions.${g}.${p}.${virus}.txt || true )
+		
+						echo $region
+						region=${region#Samtools uncommon regions: }
+						#common_regions.D13784.1.txt:Samtools uncommon regions: D13784.1:1-4163 D13784.1:4208-7649 D13784.1:7691-8000 D13784.1:8053-1000000
+			
+						echo "${region}"
+						[ -z "${region}" ] && region="${virus}"
+			
+						samtools view -c -F 4 ${subject}/${subject}.virii.bam ${region} > ${f}
+			
+						chmod a-w ${f}
+					fi
+		
+					if [ -z $( ${sql} "SELECT ${v} FROM subjects WHERE subject = '${subject}'" ) ] ; then
+						count=$( cat ${f} )
+						command="UPDATE subjects SET uncommon_${v} = '${count}' WHERE subject = '${subject}'"
+						echo "${command}"
+						${sql} "${command}"
+					fi
+
+				fi
+
+			done
 
 		done
 
-
-
-
-
-
-
-
-
-
-		if [ -f bowtie2.mapped.ratio_unmapped.txt/${subject}.${virus}.bowtie2.mapped.ratio_unmapped.txt ] && [ ! -w bowtie2.mapped.ratio_unmapped.txt/${subject}.${virus}.bowtie2.mapped.ratio_unmapped.txt ] ; then
-			echo "Write-protected ${subject}.${virus}.bowtie2.mapped.ratio_unmapped.txt exists. Skipping step."
-		else
-			echo "Calculating ratio ${virus} bowtie2 alignments to total unaligned reads"
-			echo "scale=9; "$(cat bowtie2.mapped.count.txt/${subject}.${virus}.bowtie2.mapped.count.txt)"/"$(cat unmapped.count.txt/${subject}.unmapped.count.txt) | bc > bowtie2.mapped.ratio_unmapped.txt/${subject}.${virus}.bowtie2.mapped.ratio_unmapped.txt
-			chmod a-w bowtie2.mapped.ratio_unmapped.txt/${subject}.${virus}.bowtie2.mapped.ratio_unmapped.txt
-		fi
-
-		if [ -f bowtie2.mapped.ratio_total.txt/${subject}.${virus}.bowtie2.mapped.ratio_total.txt ] && [ ! -w bowtie2.mapped.ratio_total.txt/${subject}.${virus}.bowtie2.mapped.ratio_total.txt ] ; then
-			echo "Write-protected ${subject}.${virus}.bowtie2.mapped.ratio_total.txt exists. Skipping step."
-		else
-			echo "Calculating ratio ${virus} bowtie2 alignments to total reads"
-			echo "scale=9; "$(cat bowtie2.mapped.count.txt/${subject}.${virus}.bowtie2.mapped.count.txt)"/"$(cat total.count.txt/${subject}.total.count.txt) | bc > bowtie2.mapped.ratio_total.txt/${subject}.${virus}.bowtie2.mapped.ratio_total.txt
-			chmod a-w bowtie2.mapped.ratio_total.txt/${subject}.${virus}.bowtie2.mapped.ratio_total.txt
-		fi
+#		if [ -f bowtie2.mapped.ratio_unmapped.txt/${subject}.${virus}.bowtie2.mapped.ratio_unmapped.txt ] && [ ! -w bowtie2.mapped.ratio_unmapped.txt/${subject}.${virus}.bowtie2.mapped.ratio_unmapped.txt ] ; then
+#			echo "Write-protected ${subject}.${virus}.bowtie2.mapped.ratio_unmapped.txt exists. Skipping step."
+#		else
+#			echo "Calculating ratio ${virus} bowtie2 alignments to total unaligned reads"
+#			echo "scale=9; "$(cat bowtie2.mapped.count.txt/${subject}.${virus}.bowtie2.mapped.count.txt)"/"$(cat unmapped.count.txt/${subject}.unmapped.count.txt) | bc > bowtie2.mapped.ratio_unmapped.txt/${subject}.${virus}.bowtie2.mapped.ratio_unmapped.txt
+#			chmod a-w bowtie2.mapped.ratio_unmapped.txt/${subject}.${virus}.bowtie2.mapped.ratio_unmapped.txt
+#		fi
+#
+#		if [ -f bowtie2.mapped.ratio_total.txt/${subject}.${virus}.bowtie2.mapped.ratio_total.txt ] && [ ! -w bowtie2.mapped.ratio_total.txt/${subject}.${virus}.bowtie2.mapped.ratio_total.txt ] ; then
+#			echo "Write-protected ${subject}.${virus}.bowtie2.mapped.ratio_total.txt exists. Skipping step."
+#		else
+#			echo "Calculating ratio ${virus} bowtie2 alignments to total reads"
+#			echo "scale=9; "$(cat bowtie2.mapped.count.txt/${subject}.${virus}.bowtie2.mapped.count.txt)"/"$(cat total.count.txt/${subject}.total.count.txt) | bc > bowtie2.mapped.ratio_total.txt/${subject}.${virus}.bowtie2.mapped.ratio_total.txt
+#			chmod a-w bowtie2.mapped.ratio_total.txt/${subject}.${virus}.bowtie2.mapped.ratio_total.txt
+#		fi
 
 	done
 
 done
+
+
+for virus in /raid/refs/fasta/virii/*fasta ; do
+	virus=$( basename $virus .fasta )
+	virus=${virus/./_}
+	command="UPDATE subjects SET ${virus}_unmapped = 1.0 * ${virus} / unmapped;"
+	echo "${command}"
+	${sql} "${command}"
+	command="UPDATE subjects SET ${virus}_total = 1.0 * ${virus} / total;"
+	echo "${command}"
+	${sql} "${command}"
+	command="UPDATE subjects SET uncommon_${virus}_unmapped = 1.0 * uncommon_${virus} / unmapped;"
+	echo "${command}"
+	${sql} "${command}"
+	command="UPDATE subjects SET uncommon_${virus}_total = 1.0 * uncommon_${virus} / total;"
+	echo "${command}"
+	${sql} "${command}"
+done
+
+
 
 echo "---"
 echo "Done"
