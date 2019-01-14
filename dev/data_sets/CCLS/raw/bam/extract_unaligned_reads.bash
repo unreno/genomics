fcdf1ca23c1e213b764644b8a30347145364dde0
Jake <github@jakewendt.com>
2019-01-14 12:57:01 -0800

    Preparing script to extract fasta from bam.

---
+++ dev/data_sets/CCLS/raw/bam/extract_unaligned_reads.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+for bam in *bam ; do
+	base=${bam%.recaled.bam}
+	echo $bam $base
+
+	f="${base}.fasta.gz"
+	if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+
+		echo "${f} already exists, so skipping."
+
+	else
+
+		echo "Extracting ${f} from ${bam}"
+		samtools fasta -N -f 4 --threads 38 ${bam} | gzip --best > ${f} 2> ${f}.STDERR
+
+		chmod a-w ${f}
+
+	fi
+
+done
