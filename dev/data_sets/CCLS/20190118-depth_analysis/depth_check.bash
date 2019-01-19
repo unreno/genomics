a1350224b2c9531021b0e07b20e9d3e8b9a27f74
Jake <github@jakewendt.com>
2019-01-18 17:34:41 -0800

    Meddling with the idea of insertion signature being visible in the variation of depth int bam file.

---
+++ dev/data_sets/CCLS/20190118-depth_analysis/depth_check.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+
+#samtools depth -a -r 4:9601586-9601646 186069.recaled.PP.bam | awk '
+#samtools depth -a -r 12:43000000-44000000 186069.recaled.PP.bam | awk '
+#samtools depth -a -r 12:43000000-44000000 186069.recaled.bam | awk '
+
+for f in /raid/data/raw/CCLS_983899/*983899.hg38.num*bam /raid/data/raw/CCLS/bam/*983899.recaled*bam ; do
+	echo $f
+	samtools depth -a -r 1 $f | awk '
+		BEGIN{
+			FS="\t";
+			c=100;
+			overlap=0;
+		}
+		( $3 < 100 && $3 < c-10 ){
+			#	overlap ended
+			if( overlap && overlap > 6 && overlap < 9 ){
+				print zero
+				print first
+				print last
+				print $0
+				print ""
+			}
+			overlap=0;
+		}
+		( $3 < 100 && $3 > c+10 ){
+			#	overlap started
+			overlap=1;
+			zero=previous
+			first=$0
+		}
+		( overlap ){
+			last=$0
+			overlap+=1
+		}
+		{
+			c=$3
+			previous=$0
+		}'
+done
