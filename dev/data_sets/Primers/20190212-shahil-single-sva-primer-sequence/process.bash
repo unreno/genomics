1a694a88ea90641bc8ad28a392bb8a4c3407531f
Jake <github@jakewendt.com>
2019-02-12 09:56:43 -0800

    Compared another primer

---
+++ dev/data_sets/Primers/20190212-shahil-single-sva-primer-sequence/process.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+
+for ref in SVA_A SVA_B SVA_C SVA_D SVA_E SVA_F ; do
+	echo $ref
+
+	#	These reads overlap by 1, which by default would make them not a proper pair.
+	#	Not entirely relevant here, but the --dovetail option alleviates this restriction.
+	bowtie2 --all --ff --dovetail --xeq -f -x $ref --very-sensitive -1 sva_primer.10.fa -2 sva_primer.11.fa -S $ref.sam
+
+done
+
+bowtie2 --ff -X 50 --all --dovetail --xeq -f -x hg38_no_alts --very-sensitive -1 sva_primer.10.fa -2 sva_primer.11.fa > SVA_10-11.sam 
+
