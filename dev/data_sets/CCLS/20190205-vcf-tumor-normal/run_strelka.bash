c9429585f95f86a87d8183ea209ca5fcd74649d0
Jake <github@jakewendt.com>
2019-02-21 19:32:57 -0800

    Still analyzing 983899 related FASTQ/BAM/VCF.

--- dev/data_sets/CCLS/20190205-vcf-tumor-normal/run_strelka.bash
+++ dev/data_sets/CCLS/20190205-vcf-tumor-normal/run_strelka.bash
@@ -10,82 +10,98 @@ wd=$PWD
 #	Both the provided bams and my bams were done on hg38, no chr prefix, no alts.
 mkdir -p strelka
 
-#for base in 268325 439338 63185 634370 983899 ; do
-#
-#	runDir="strelka/${base}.loc"
-#
-#	~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
-#		--normalBam /raid/data/raw/CCLS/bam/GM_${base}.recaled.bam \
-#		--tumorBam /raid/data/raw/CCLS/bam/${base}.recaled.bam \
-#		--ref /raid/refs/fasta/hg38.num.fa.gz \
-#		--runDir ${runDir}
-#
-#	${runDir}/runWorkflow.py -m local -j 40
-#
-#done
+for base in 268325 439338 63185 634370 983899 ; do
+
+	runDir="strelka/${base}.hg38_num_noalts.loc"
+	if [ -e $runDir ] ; then
+		echo "$runDir exists. Skipping."
+	else
+		echo "Running Strelka Somatic Workflow in $runDir"
+		~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
+			--normalBam /raid/data/raw/CCLS/bam/GM_${base}.recaled.bam \
+			--tumorBam /raid/data/raw/CCLS/bam/${base}.recaled.bam \
+			--ref /raid/refs/fasta/hg38.num.fa.gz \
+			--runDir ${runDir}
+		${runDir}/runWorkflow.py -m local -j 40
+	fi
+
+done
 
 
 base=983899
 
 
-#runDir="strelka/${base}.loc.PP"
-#
-#~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
-#	--normalBam /raid/data/raw/CCLS/bam/GM_${base}.recaled.PP.bam \
-#	--tumorBam /raid/data/raw/CCLS/bam/${base}.recaled.PP.bam \
-#	--ref /raid/refs/fasta/hg38.num.fa.gz \
-#	--runDir ${runDir}
-#
-#${runDir}/runWorkflow.py -m local -j 40
+runDir="strelka/${base}.hg38_num_noalts.loc_PP"
+if [ -e $runDir ] ; then
+	echo "$runDir exists. Skipping."
+else
+	echo "Running Strelka Somatic Workflow in $runDir"
+	~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
+		--normalBam /raid/data/raw/CCLS/bam/GM_${base}.recaled.PP.bam \
+		--tumorBam /raid/data/raw/CCLS/bam/${base}.recaled.PP.bam \
+		--ref /raid/refs/fasta/hg38.num.fa.gz \
+		--runDir ${runDir}
+	${runDir}/runWorkflow.py -m local -j 40
+fi	
 
 
-runDir="strelka/${base}.e2e"
-
-~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
-	--normalBam /raid/data/raw/CCLS_983899/bam/GM_${base}.hg38.num.bam \
-	--tumorBam /raid/data/raw/CCLS_983899/bam/${base}.hg38.num.bam \
-	--ref /raid/refs/fasta/hg38.num.fa.gz \
-	--runDir ${runDir}
-
-${runDir}/runWorkflow.py -m local -j 40
-
-
-runDir="strelka/${base}.e2e.PP"
-
-~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
-	--normalBam /raid/data/raw/CCLS_983899/bam/GM_${base}.hg38.num.PP.bam \
-	--tumorBam /raid/data/raw/CCLS_983899/bam/${base}.hg38.num.PP.bam \
-	--ref /raid/refs/fasta/hg38.num.fa.gz \
-	--runDir ${runDir}
-
-${runDir}/runWorkflow.py -m local -j 40
+runDir="strelka/${base}.hg38_num_noalts.e2e"
+if [ -e $runDir ] ; then
+	echo "$runDir exists. Skipping."
+else
+	echo "Running Strelka Somatic Workflow in $runDir"
+	~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
+		--normalBam /raid/data/raw/CCLS_983899/bam/GM_${base}.hg38_num_noalts.e2e.bam \
+		--tumorBam /raid/data/raw/CCLS_983899/bam/${base}.hg38_num_noalts.e2e.bam \
+		--ref /raid/refs/fasta/hg38.num.fa.gz \
+		--runDir ${runDir}
+	${runDir}/runWorkflow.py -m local -j 40
+fi
 
 
-
-#runDir="strelka/${base}.alts.e2e"
-#
-#~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
-#	--normalBam /raid/data/raw/CCLS_983899/bam/GM_${base}.hg38.bam \
-#	--tumorBam /raid/data/raw/CCLS_983899/bam/${base}.hg38.bam \
-#	--ref /raid/refs/fasta/hg38.fa.gz \
-#	--runDir ${runDir}
-#
-#${runDir}/runWorkflow.py -m local -j 40
-#
-#
-#runDir="strelka/${base}.alts.e2e.PP"
-#
-#~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
-#	--normalBam /raid/data/raw/CCLS_983899/bam/GM_${base}.hg38.PP.bam \
-#	--tumorBam /raid/data/raw/CCLS_983899/bam/${base}.hg38.PP.bam \
-#	--ref /raid/refs/fasta/hg38.fa.gz \
-#	--runDir ${runDir}
-#
-#${runDir}/runWorkflow.py -m local -j 40
+runDir="strelka/${base}.hg38_num_noalts.e2e_PP"
+if [ -e $runDir ] ; then
+	echo "$runDir exists. Skipping."
+else
+	echo "Running Strelka Somatic Workflow in $runDir"
+	~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
+		--normalBam /raid/data/raw/CCLS_983899/bam/GM_${base}.hg38_num_noalts.e2e_PP.bam \
+		--tumorBam /raid/data/raw/CCLS_983899/bam/${base}.hg38_num_noalts.e2e_PP.bam \
+		--ref /raid/refs/fasta/hg38.num.fa.gz \
+		--runDir ${runDir}
+	${runDir}/runWorkflow.py -m local -j 40
+fi
 
 
 
-#	Testing single runs
+runDir="strelka/${base}.hg38_chr_alts.e2e"
+if [ -e $runDir ] ; then
+	echo "$runDir exists. Skipping."
+else
+	echo "Running Strelka Somatic Workflow in $runDir"
+	~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
+		--normalBam /raid/data/raw/CCLS_983899/bam/GM_${base}.hg38_chr_alts.e2e.bam \
+		--tumorBam /raid/data/raw/CCLS_983899/bam/${base}.hg38_chr_alts.e2e.bam \
+		--ref /raid/refs/fasta/hg38.fa.gz \
+		--runDir ${runDir}
+	${runDir}/runWorkflow.py -m local -j 40
+fi
+
+runDir="strelka/${base}.hg38_chr_alts.e2e_PP"
+if [ -e $runDir ] ; then
+	echo "$runDir exists. Skipping."
+else
+	echo "Running Strelka Somatic Workflow in $runDir"
+	~/.local/strelka/bin/configureStrelkaSomaticWorkflow.py \
+		--normalBam /raid/data/raw/CCLS_983899/bam/GM_${base}.hg38_chr_alts.e2e_PP.bam \
+		--tumorBam /raid/data/raw/CCLS_983899/bam/${base}.hg38_chr_alts.e2e_PP.bam \
+		--ref /raid/refs/fasta/hg38.fa.gz \
+		--runDir ${runDir}
+	${runDir}/runWorkflow.py -m local -j 40
+fi
+
+
+
 
 mkdir -p strelka_germline
 
@@ -93,49 +109,79 @@ base="983899"
 
 for base in 983899 GM_983899 ; do
 
-#	runDir="strelka_germline/${base}.alts.e2e.PP"
-#	~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
-#		--bam /raid/data/raw/CCLS_983899/bam/${base}.hg38.PP.bam \
-#		--referenceFasta /raid/refs/fasta/hg38.fa.gz \
-#		--runDir ${runDir}
-#	${runDir}/runWorkflow.py -m local -j 40
-#
-#	runDir="strelka_germline/${base}.alts.e2e"
-#	~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
-#		--bam /raid/data/raw/CCLS_983899/bam/${base}.hg38.bam \
-#		--referenceFasta /raid/refs/fasta/hg38.fa.gz \
-#		--runDir ${runDir}
-#	${runDir}/runWorkflow.py -m local -j 40
+	runDir="strelka_germline/${base}.hg38_chr_alts.e2e_PP"
+	if [ -e $runDir ] ; then
+		echo "$runDir exists. Skipping."
+	else
+		echo "Running Strelka Germline Workflow in $runDir"
+		~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
+			--bam /raid/data/raw/CCLS_983899/bam/${base}.hg38_chr_alts.e2e_PP.bam \
+			--referenceFasta /raid/refs/fasta/hg38.fa.gz \
+			--runDir ${runDir}
+		${runDir}/runWorkflow.py -m local -j 40
+	fi
+
+	runDir="strelka_germline/${base}.hg38_chr_alts.e2e"
+	if [ -e $runDir ] ; then
+		echo "$runDir exists. Skipping."
+	else
+		echo "Running Strelka Germline Workflow in $runDir"
+		~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
+			--bam /raid/data/raw/CCLS_983899/bam/${base}.hg38_chr_alts.e2e.bam \
+			--referenceFasta /raid/refs/fasta/hg38.fa.gz \
+			--runDir ${runDir}
+		${runDir}/runWorkflow.py -m local -j 40
+	fi
 
 
-#	These don't seem to filter anything out
+	#	These don't seem to filter anything out
 
-#	runDir="strelka_germline/${base}.loc"
-#	~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
-#		--bam /raid/data/raw/CCLS/bam/${base}.recaled.bam \
-#		--referenceFasta /raid/refs/fasta/hg38.num.fa.gz \
-#		--runDir ${runDir}
-#	${runDir}/runWorkflow.py -m local -j 40
-#
-#	runDir="strelka_germline/${base}.loc.PP"
-#	~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
-#		--bam /raid/data/raw/CCLS/bam/${base}.recaled.PP.bam \
-#		--referenceFasta /raid/refs/fasta/hg38.num.fa.gz \
-#		--runDir ${runDir}
-#	${runDir}/runWorkflow.py -m local -j 40
+	runDir="strelka_germline/${base}.hg38_num_noalts.loc"
+	if [ -e $runDir ] ; then
+		echo "$runDir exists. Skipping."
+	else
+		echo "Running Strelka Germline Workflow in $runDir"
+		~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
+			--bam /raid/data/raw/CCLS/bam/${base}.recaled.bam \
+			--referenceFasta /raid/refs/fasta/hg38.num.fa.gz \
+			--runDir ${runDir}
+		${runDir}/runWorkflow.py -m local -j 40
+	fi
 
-#	runDir="strelka_germline/${base}.e2e"
-#	~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
-#		--bam /raid/data/raw/CCLS_983899/bam/${base}.hg38_num_noalts.e2e.bam \
-#		--referenceFasta /raid/refs/fasta/hg38.num.fa.gz \
-#		--runDir ${runDir}
-#	${runDir}/runWorkflow.py -m local -j 40
-#
-#	runDir="strelka_germline/${base}.e2e.PP"
-#	~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
-#		--bam /raid/data/raw/CCLS_983899/bam/${base}.hg38_num_noalts.e2e_PP.bam \
-#		--referenceFasta /raid/refs/fasta/hg38.num.fa.gz \
-#		--runDir ${runDir}
-#	${runDir}/runWorkflow.py -m local -j 40
+	runDir="strelka_germline/${base}.hg38_num_noalts.loc_PP"
+	if [ -e $runDir ] ; then
+		echo "$runDir exists. Skipping."
+	else
+		echo "Running Strelka Germline Workflow in $runDir"
+		~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
+			--bam /raid/data/raw/CCLS/bam/${base}.recaled.PP.bam \
+			--referenceFasta /raid/refs/fasta/hg38.num.fa.gz \
+			--runDir ${runDir}
+		${runDir}/runWorkflow.py -m local -j 40
+	fi
+
+	runDir="strelka_germline/${base}.hg38_num_noalts.e2e"
+	if [ -e $runDir ] ; then
+		echo "$runDir exists. Skipping."
+	else
+		echo "Running Strelka Germline Workflow in $runDir"
+		~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
+			--bam /raid/data/raw/CCLS_983899/bam/${base}.hg38_num_noalts.e2e.bam \
+			--referenceFasta /raid/refs/fasta/hg38.num.fa.gz \
+			--runDir ${runDir}
+		${runDir}/runWorkflow.py -m local -j 40
+	fi
+
+	runDir="strelka_germline/${base}.hg38_num_noalts.e2e_PP"
+	if [ -e $runDir ] ; then
+		echo "$runDir exists. Skipping."
+	else
+		echo "Running Strelka Germline Workflow in $runDir"
+		~/.local/strelka/bin/configureStrelkaGermlineWorkflow.py \
+			--bam /raid/data/raw/CCLS_983899/bam/${base}.hg38_num_noalts.e2e_PP.bam \
+			--referenceFasta /raid/refs/fasta/hg38.num.fa.gz \
+			--runDir ${runDir}
+		${runDir}/runWorkflow.py -m local -j 40
+	fi
 
 done
