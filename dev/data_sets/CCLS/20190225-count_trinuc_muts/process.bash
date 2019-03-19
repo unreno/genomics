70a6b31cfdc54683bf47f5e9626fe0a752968eaf
Jake <github@jakewendt.com>
2019-03-18 21:27:38 -0700

    Trying to apply mutation signatures to CCLS data

--- dev/data_sets/CCLS/20190225-count_trinuc_muts/process.bash
+++ dev/data_sets/CCLS/20190225-count_trinuc_muts/process.bash
@@ -20,7 +20,7 @@ for vcf in /raid/data/raw/CCLS/vcf/*.output-HC.vcf.gz ; do
 	if [ -f ${f} ] && [ ! -w ${f} ]  ; then
 		echo "Write-protected ${f} exists. Skipping step."
 	else
-		bcftools query --include 'TYPE="snp" && QD > 2 && FS < 60 && MQ > 40 && MQRankSum > -12.5 && ReadPosRankSum > -8.0' -f "%CHROM\t%POS\t%REF\t%ALT\t+\n" ${vcf} > ${f}
+		bcftools query --include 'TYPE="snp" && QD > 2 && FS < 60 && MQ > 40 && MQRankSum > -12.5 && ReadPosRankSum > -8.0' -f "%CHROM\t%POS\t%REF\t%ALT\t+\t${base}\n" ${vcf} > ${f}
 		chmod a-w ${f}
 	fi
 
@@ -28,9 +28,26 @@ for vcf in /raid/data/raw/CCLS/vcf/*.output-HC.vcf.gz ; do
 	if [ -f ${f} ] && [ ! -w ${f} ]  ; then
 		echo "Write-protected ${f} exists. Skipping step."
 	else
-		./count_trinuc_muts_v8.pl vcf /raid/refs/fasta/hg38_num_noalts.fa ${base}.count_trinuc_muts.input.txt
+		#./count_trinuc_muts_v8.pl vcf /raid/refs/fasta/hg38_num_noalts.fa ${base}.count_trinuc_muts.input.txt
+		#	pvcf trigger sample name extraction
+		./count_trinuc_muts_v8.pl pvcf /raid/refs/fasta/hg38_num_noalts.fa ${base}.count_trinuc_muts.input.txt
 		mv ${base}.count_trinuc_muts.input.txt.*.count.txt ${f}
 		chmod a-w ${f}
 	fi
 
+
 done
+
+
+f=mut_all_sort.txt
+if [ -f ${f} ] && [ ! -w ${f} ]  ; then
+	echo "Write-protected ${f} exists. Skipping step."
+else
+	echo "Head *count_trinuc_muts.txt"
+	head -1 -q *count_trinuc_muts.txt | head -1 > ${f}
+	echo "Combining and sorting *count_trinuc_muts.txt"
+	tail -n +2 *count_trinuc_muts.txt | sort -k1,1 -k2,2n >> ${f}
+	chmod a-w ${f}
+fi
+
+
