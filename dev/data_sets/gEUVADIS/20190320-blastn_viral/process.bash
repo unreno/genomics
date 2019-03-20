58202a86af5756e6aa7ac5bad69b96b811faf1cb
Jake <github@jakewendt.com>
2019-03-20 09:14:48 -0700

    Deving pipeline to blast all geuvadis data.

--- dev/data_sets/gEUVADIS/20190320-blastn_viral/process.bash
+++ dev/data_sets/gEUVADIS/20190320-blastn_viral/process.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#ls *fasta | parallel --no-notice --joblog ~/parallel.1.log -j40 blastn -query {} -num_threads 1 -outfmt 6 -db viral.masked -out {}.viral.masked.1.txt 2\> {}.viral.masked.1.err &
+#ls *fasta | parallel --no-notice --joblog ~/parallel.1.log -j20 blastn -query {} -num_threads 1 -outfmt 6 -db viral.masked -out {}.viral.masked.1.txt 2\> {}.viral.masked.1.err &
 
-#ls /raid/data/raw/gEUVADIS/*fastq.gz | parallel --no-notice --joblog ./parallel.joblog -j40 ./process.bash &
+#ls /raid/data/raw/gEUVADIS/*fastq.gz | parallel --no-notice --joblog ./parallel.joblog -j20 ./process.bash &
 
 
 set -e	#	exit if any command fails
@@ -18,39 +18,40 @@ base=$( basename $1 .fastq.gz )
 
 fastq_count=${base}.fastq_count
 if [ -f ${fastq_count} ] && [ ! -w ${fastq_count} ]  ; then
-	echo "${fastq_count} already exists, so skipping."
+	echo "${fastq_count} already exists, so skipping." >> ${base}.log
 else
-	echo "Creating ${fastq_count}"
-	fastq_line_count=$( wc -l <( zcat $1 ) )
+	echo "Creating ${fastq_count}" >> ${base}.log
+	#fastq_line_count=$( wc -l <( zcat $1 ) )	#	Don't do it this way
+	fastq_line_count=$( zcat $1 | wc -l )
 	echo "${fastq_line_count}/4" | bc > ${fastq_count}
 	chmod a-w ${fastq_count}
 fi
 
-fasta=${base}.fasta
-if [ -f ${fasta} ] && [ ! -w ${fasta} ]  ; then
-	echo "${fasta} already exists, so skipping."
-else
-	echo "Creating ${fasta}"
-	fastqToFa <( zcat $1 ) ${fasta}
-	chmod a-w ${fasta}
-fi
-
-fasta_count=${base}.fasta_count
-if [ -f ${fasta_count} ] && [ ! -w ${fasta_count} ]  ; then
-	echo "${fasta_count} already exists, so skipping."
-else
-	echo "Creating ${fasta_count}"
-	grep -c "^>" ${fasta} > ${fasta_count}
-	chmod a-w ${fasta_count}
-fi
-
-blast_out="${base}.viral.masked.txt"
-blast_err="${base}.viral.masked.err"
-if [ -f ${blast_out} ] && [ ! -w ${blast_out} ]  ; then
-	echo "${blast_out} already exists, so skipping."
-else
-	echo "Blasting ${fasta} creating ${blast_out}"
-	blastn -query ${fasta} -num_threads 1 -outfmt 6 -db viral.masked -out ${blast_out} 2> ${blast_err}
-	chmod a-w ${blast_out}
-fi
+#	fasta=${base}.fasta
+#	if [ -f ${fasta} ] && [ ! -w ${fasta} ]  ; then
+#		echo "${fasta} already exists, so skipping." >> ${base}.log
+#	else
+#		echo "Creating ${fasta}" >> ${base}.log
+#		fastqToFa <( zcat $1 ) ${fasta}
+#		chmod a-w ${fasta}
+#	fi
+#	
+#	fasta_count=${base}.fasta_count
+#	if [ -f ${fasta_count} ] && [ ! -w ${fasta_count} ]  ; then
+#		echo "${fasta_count} already exists, so skipping." >> ${base}.log
+#	else
+#		echo "Creating ${fasta_count}" >> ${base}.log
+#		grep -c "^>" ${fasta} > ${fasta_count}
+#		chmod a-w ${fasta_count}
+#	fi
+#	
+#	blast_out="${base}.viral.masked.txt"
+#	blast_err="${base}.viral.masked.err"
+#	if [ -f ${blast_out} ] && [ ! -w ${blast_out} ]  ; then
+#		echo "${blast_out} already exists, so skipping." >> ${base}.log
+#	else
+#		echo "Blasting ${fasta} creating ${blast_out}" >> ${base}.log
+#		blastn -query ${fasta} -num_threads 2 -outfmt 6 -db viral.masked -out ${blast_out} 2> ${blast_err}
+#		chmod a-w ${blast_out}
+#	fi
 
