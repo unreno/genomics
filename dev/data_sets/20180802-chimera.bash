f01cd014da99f3cc2ed6d73f4dcb01671566b74d
Jake <github@jakewendt.com>
2019-04-23 07:23:22 -0700

    Included common script

---
+++ dev/data_sets/20180802-chimera.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo $0
+
+project=$( basename $PWD )
+echo $project
+
+for virus in HERV_K113 SVA_A SVA_B SVA_C SVA_D SVA_E SVA_F ; do
+echo $virus
+
+mkdir 20180802-chimera-${virus}-hg38_no_alts
+cd 20180802-chimera-${virus}-hg38_no_alts
+
+#	CEPH-ENA-PRJEB3246
+#	CEPH-ENA-PRJEB3381
+#	CEPH-ENA-PRJNA186949
+
+#command chimera.bash --human hg38_no_alts --viral ${virus} --threads 4 /raid/data/raw/${project}/*.fastq.gz
+chimera.bash --human hg38_no_alts --viral ${virus} --threads 4 /raid/data/raw/${project}/*.fastq.gz &
+
+cd ..
+
+done
+
