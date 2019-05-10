178642dbd25cdff5e87e471a05d4ff430ab2df8c
Jake <github@jakewendt.com>
2019-05-10 07:36:01 -0700

    Cleanup

--- scripts/allele_ratio_histogram.py
+++ scripts/allele_ratio_histogram.py
@@ -16,19 +16,14 @@ plt.rcParams["figure.figsize"] = [18.0,8.0]
 
 file_name = sys.argv[1]
 
-#print(file_name)
-
 file_name_parts=file_name.split('/')
-#print(file_name_parts[len(file_name_parts)-1])
-basename=file_name_parts[len(file_name_parts)-1]
 
+base_name=file_name_parts[len(file_name_parts)-1]
 
 ratios = pd.read_csv(file_name,sep='\t',
 	usecols=['REF_RATIO','ALT1_RATIO','ALT2_RATIO','ALT3_RATIO'])
 
-ratios.plot(kind='hist',title=basename,bins=200,alpha=0.3,logy=True)
+ratios.plot(kind='hist',title=base_name,bins=200,alpha=0.3,logy=True)
 
 plt.savefig(file_name+'.histogram.png')
-
-
 
