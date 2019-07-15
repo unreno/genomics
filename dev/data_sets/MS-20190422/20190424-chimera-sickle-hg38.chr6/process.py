37e933e47b9cc53c790f44442822cd641b2d410a
Jake <github@jakewendt.com>
2019-07-15 08:43:51 -0700

    Attempting output to VCF file.

--- dev/data_sets/MS-20190422/20190424-chimera-sickle-hg38.chr6/process.py
+++ dev/data_sets/MS-20190422/20190424-chimera-sickle-hg38.chr6/process.py
@@ -36,7 +36,7 @@ for key in groups.keys():
 	
 				dfs.append(df)
 	
-				print("Raw shape")
+				print('Raw shape')
 				print(df.shape)
 
 				grouped_df = df.groupby( [pd.cut(df['position'], np.arange(0, 180000000+1, 10000)),'hkle','status','direction'] ).sum()
@@ -45,7 +45,7 @@ for key in groups.keys():
 
 				print(grouped_df.head())
 				grouped_dfs.append(grouped_df)
-				print("Grouped shape")
+				print('Grouped shape')
 				print(grouped_df.shape)
 
 	
@@ -67,6 +67,36 @@ for key in groups.keys():
 	print(all.head(50))
 	
 	all.to_csv('hkle_'+key+'.csv')
+
+
+
+
+	#	OUTPUT TO VCF
+
+##fileformat=VCFv4.1
+##FILTER=<ID=PASS,Description="All filters passed">
+##fileDate=20150218
+##reference=ftp://ftp.1000genomes.ebi.ac.uk//vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
+##source=1000GenomesPhase3Pipeline
+
+#	#CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  HG00096 HG00097 HG00099 HG00100 HG00101
+#	22      36962078        rs4359745       A       G       100     PASS    AC=150;AF=0.0299521;AN=5008;NS=2504;DP=20576;EAS_AF=0;AMR_AF=0.0086;AFR_AF=0.1089;EUR_AF=0;SAS_AF=0;AA=A|||;VT=SNP      GT      0|0     0|0     0|0     0|0     0|0
+
+	vcf = pd.DataFrame(data={'#CHROM': [],'POS': [], 'ID':[], 'REF':[],
+		'ALT':[], 'QUAL':[], 'FILTER':[], 'INFO':[], 'FORMAT':[]})
+	vcf['POS'] = all['pos']
+	vcf['#CHROM'] = 'chr6'
+	vcf['ID'] = '.'
+	vcf['REF'] = '.'
+	vcf['ALT'] = '.'
+	vcf['QUAL'] = 0
+	vcf['FILTER'] = '.'
+	vcf['INFO'] = '.'
+	vcf['FORMAT'] = 'GT'
+
+
+	all.to_csv('hkle_'+key+'.vcf', sep='\t', index=False)
+
 
 
 	all=pd.concat(grouped_dfs,sort=True)
