2a2198fe84a5a1186eba6b60068732d6573418b5
Jake <github@jakewendt.com>
2021-01-29 14:31:17 -0800

    Cleaned up links to files in this repo

--- admin_scripts/relink_to_c4.bash
+++ admin_scripts/relink_to_c4.bash
@@ -5,9 +5,17 @@
 while read f ; do
 	link=$( readlink -m "$f" )
 	#if [[ ${link} == "/home/gwendt/github/ucsffrancislab/genomics"* ]] ; then
+	if [[ ${link} == "/c4/home/gwendt/github/unreno/unreno"* ]] ; then
+		link=${link/\/c4\/home\/gwendt\/github\/unreno\/unreno/\/c4\/home\/gwendt\/github\/unreno}
+		echo chmod +w $( dirname "${f}" )
+		echo rm -f "${f}"
+		echo ln -s "${link}" "${f}"
+	fi
 	if [[ ${link} == "/home/jake/.github/unreno/genomics"* ]] ; then
 		#echo $f
 		#echo $link
+		#link=${link/\/home\/jake\/.github\/unreno\/genomics\/scripts/\/home\/gwendt\/github\/unreno\/unreno\/genomics\/dev}
+		link=${link/\/home\/jake\/.github\/unreno\/genomics\/scripts/\/home\/gwendt\/github\/unreno\/genomics\/dev}
 		link=${link/\/home\/jake\/.github/\/home\/gwendt\/github}
 		echo chmod +w $( dirname "${f}" )
 		echo rm -f "${f}"
@@ -16,7 +24,10 @@ while read f ; do
 #done
 #done < <( find /francislab/data1/refs/ -type l )
 #done < <( find /francislab/data1/ -type l )
-done < <( find -L /francislab/data1/ -type l )
+done < <( find -L /francislab/data1/working/ -type l )
+#done < <( find -L /francislab/data1/working/CCLS/ -type l )
+#done < <( find -L /francislab/data1/working/1000genomes/ -type l )
+#done < <( find -L /francislab/data1/working/CCLS_983899/ -type l )
 
 #	using the while loop seems to work better than the for loop
 #	I think that the for loop won't execute the first iteration until the find command completes.
